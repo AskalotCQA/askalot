fbeb0200c9c2883a990ecc1ca14d57d35ce9e4bc
Pavol Zbell <pavol.zbell@gmail.com>
2013-10-09 22:41:40 +0200

    Generate Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NaRuby::Application.config.secret_key_base = '453c1f2134371c272f8f137685a76f6360f147d2422d718ce9fe6ac526736bb4fbb60697b0c80d243390bd8cc01621e8619e2be60c9173a393b2e530287d6a07'
