bf68f2f14a000aeb54fb2a48cf46a4d2e521307a
Pavol Zbell <pavol.zbell@gmail.com>
2013-10-10 06:39:39 +0200

    Add basic application configuration

---
+++ app/models/configuration.rb
@@ -0,0 +1,7 @@
+class Configuration
+  include Squire::Base
+
+  squire.source Rails.root.join('config', 'configuration.yml')
+
+  squire.namespace Rails.env.to_sym, base: :defaults
+end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-NaRuby::Application.config.secret_key_base = '453c1f2134371c272f8f137685a76f6360f147d2422d718ce9fe6ac526736bb4fbb60697b0c80d243390bd8cc01621e8619e2be60c9173a393b2e530287d6a07'
+NaRuby::Application.config.secret_key_base = ::Configuration.secret_token
