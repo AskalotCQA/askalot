9a095dd6d6e9b9fef639108571bb8862dbc3a60d
Samuel Molnar <molnar.samuel@gmail.com>
2013-10-13 00:59:41 +0200

    Add locale and ActionMailer defaults

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,9 @@ NaRuby::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.delivery_method     = :test
+  config.action_mailer.default_url_options = { host: 'www.example.com' }
+
+  config.i18n.default_locale = :en
 end
