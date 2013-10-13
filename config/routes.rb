f0b3693cdfca9b3cca2a74a020f0ef184977cfdb
Pavol Zbell <pavol.zbell@gmail.com>
2013-10-13 16:50:13 +0200

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 NaRuby::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'static_pages#home'
+
+  devise_for :users
+
+  match 'users/:login', via: :get, to: 'users#show', as: :user
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,7 +53,4 @@ NaRuby::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  devise_for :users
-
-  match 'users/:login', via: :get, to: 'users#show', as: :user
 end
