3e274f4e60a1ee79b7cf74bb0ce2e505357fd039
Pavol Zbell <pavol.zbell@gmail.com>
2013-10-14 01:17:46 +0200

    Fix Devise helper and some bugs

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,6 +1,9 @@
 module DeviseHelper
   def devise_error_messages!
-    flash.now[:error] = resource.errors.full_messages
+    messages = resource.errors.full_messages
+
+    flash.now[:error] = Array.wrap(flash.now[:error]) + messages unless messages.empty?
+
     nil
   end
 end
