d985d3927623b6da798046ff6cf9e9931e000323
Pavol Zbell <pavol.zbell@gmail.com>
2013-10-13 14:14:25 +0200

    Add application title helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def default_title
+    'na&#x042f;uby'
+  end
+
+  def resolve_title(value)
+    return default_title if value.blank?
+    return title(value) unless value.end_with? default_title
+
+    value
+  end
+
+  def title(*values)
+    (values << default_title).map { |value| html_escape value }.join(' &middot; ').html_safe
+  end
 end
