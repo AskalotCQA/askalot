f17550f41c8aa9c77c2e7fa8a53118cdcd93320a
Pavol Zbell <pavol.zbell@gmail.com>
2013-10-13 12:51:30 +0200

    Fix DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,3 +11,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20131012191904) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "events", force: true do |t|
+    t.json     "data",       null: false
+    t.datetime "created_ad", null: false
+  end
+
+  add_index "events", ["created_ad"], name: "index_events_on_created_ad", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "login",                               null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["login"], name: "index_users_on_login", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+end
