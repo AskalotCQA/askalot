47805fd9f72c5e4fa3ae9acea19c67add0c03f9f
Samuel Molnar <molnar.samuel@gmail.com>
2013-10-13 12:29:48 +0200

    Create dumb example for resource authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  # TODO (smolnar) use locales for message, refactor
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:alert] = exception.message
+
+    redirect_to root_url
+  end
+
   protected
 
   def configure_permitted_parameters
