a7f953cca1a11300a4fb69f299b1365a9670ef5c
Samuel Molnar <molnar.samuel@gmail.com>
2013-10-14 02:25:29 +0200

    Add specs for custom login authentification method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,37 @@
 require 'spec_helper'
 
 describe User do
+  let(:user) { create :user }
+
+  it 'should require login' do
+    user = build :user, login: ''
+
+    user.should_not be_valid
+
+    user = build :user, login: 'tra-lala'
+
+    user.should_not be_valid
+  end
+
+  describe '.find_first_by_auth_conditions' do
+    context 'when providing login' do
+      it 'should find user' do
+        other = User.find_first_by_auth_conditions(login: user.login)
+
+        other.should eql(user)
+
+        other = User.find_first_by_auth_conditions(login: user.login.upcase)
+
+        other.should eql(user)
+      end
+    end
+
+    context 'when providing email' do
+      it 'should find user' do
+        other = User.find_first_by_auth_conditions(login: user.email)
+
+        other.should eql(user)
+      end
+    end
+  end
 end
