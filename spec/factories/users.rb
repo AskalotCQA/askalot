279c009300dbe715292f53e84a5136823b82eeeb
Samuel Molnar <molnar.samuel@gmail.com>
2013-10-14 02:24:39 +0200

    Update factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,16 +2,18 @@
 
 FactoryGirl.define do
   factory :user do
-    sequence(:login) { |n| "User ##{n}" }
+    sequence(:login) { |n| "user_#{n}" }
     sequence(:email) { |n| "user#{n}@gmail.com" }
 
     password              'password'
     password_confirmation 'password'
 
-    # TODO (smolnar) consider using traits for confirmation
-    # like create :user, :confirmed
     after :create do |user|
       user.confirm!
+    end
+
+    trait :unconfirmed do
+      confirmed_at nil
     end
   end
 end
