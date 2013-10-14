d7d2bfcb2c94d66a3bdc183a3a3abf59c92557f8
Samuel Molnar <molnar.samuel@gmail.com>
2013-10-14 02:19:32 +0200

    Authenticate user in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
   def show
     @user = User.find_by_login(params[:login])
-
-    authorize! :read, @user
   end
 end
