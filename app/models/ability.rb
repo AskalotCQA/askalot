a0ecb5587682c79d8bb571790c0850b4b6487f74
Samuel Molnar <molnar.samuel@gmail.com>
2013-10-14 02:19:49 +0200

    Remove abilities for now

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,5 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, User do
-      user.present?
-    end
-
-    can :edit, User do |u|
-      u == user
-    end
   end
 end
