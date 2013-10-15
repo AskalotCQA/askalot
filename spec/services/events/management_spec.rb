630e58bb74a359e32d9a82be27863fc891b4d3a8
Pavol Zbell <pavol.zbell@gmail.com>
2013-10-15 23:41:20 +0200

    Refactor event management service tests

--- spec/services/events/management_spec.rb
+++ spec/services/events/management_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe Events::Management do
   describe '#log' do
+    let(:management) { Events::Management.new }
+
     let(:request) do
       double :request, {
         fullpath: "/users/xuser",
@@ -26,10 +28,16 @@ describe Events::Management do
     end
 
     let(:params) do
-      double :params, {
-        controller: "users",
-        action: "show",
-        login: "xuser"
+      {
+        utf8: "✓",
+        authenticity_token: "3e1a905ab3b7e5985d31badc40ca61dc",
+        user: {
+          login: "xuser",
+          password: "12345678",
+          remember_me: "0"
+        },
+        action: "create",
+        controller: "sessions"
       }
     end
 
@@ -42,20 +50,51 @@ describe Events::Management do
     end
 
     it 'creates event' do
-      management = Events::Management.new request: request, params: params, user: user
-      event      = management.log({ action: 'hello' })
+      data  = { action: 'hello', snapshot: { request: request, params: params }}
+      event = management.log data
 
       expect(event.data[:action]).to eql 'hello'
+      expect(event.data[:request][:method]).to eql request.method
+      expect(event.data[:session][:id]).to eql request.session_options[:id]
+      expect(event.data[:params][:controller]).to eql params[:controller]
+    end
+
+    it 'creates event with snapshot user' do
+      data  = { action: 'hello', snapshot: { request: request, params: params, user: user }}
+      event = management.log data
+
+      expect(event.data[:user][:id]).to eql user.id
+    end
+
+    it 'creates event with custom user' do
+      data  = { action: 'hello', user: { key: 'hello' }, snapshot: { request: request, params: params, user: user }}
+      event = management.log data
+
+      expect(event.data[:user][:key]).to eql 'hello'
     end
 
     it 'fails on missing action' do
-      management = Events::Management.new request: request, params: params, user: user
+      data = { snapshot: { request: request, params: params }}
 
-      expect { management.log({}) }.to raise_error RuntimeError
+      expect { management.log(data) }.to raise_error
+    end
+
+    it 'fails on missing snapshot request or params' do
+      expect { management.log({ action: 'hello' }) }.to raise_error
+      expect { management.log({ action: 'hello', snapshot: { request: request }}) }.to raise_error
+      expect { management.log({ action: 'hello', snapshot: { params:  params  }}) }.to raise_error
+    end
+
+    it 'fails on custom user but no snapshot user' do
+      expect { management.log({ action: 'hello', user: {}, snapshot: { request: request, params: params }}) }.to raise_error
     end
 
     it 'secures passwords and tokens' do
-      pending
+      data  = { action: 'hello', snapshot: { request: request, params: params }}
+      event = management.log data
+      
+      expect(event.data[:params][:authenticity_token]).to eql :__SECURED__
+      expect(event.data[:params][:user][:password]).to eql :__SECURED__
     end
   end
 end
