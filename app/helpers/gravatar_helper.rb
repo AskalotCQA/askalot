2f3c9516b2bf48448468d17fda9a67b55232d25c
MarekGrznar <marek.grznar@gmail.com>
2013-10-23 08:18:05 +0200

    Add Gravatar image tag helper

--- app/helpers/gravatar_helper.rb
+++ app/helpers/gravatar_helper.rb
@@ -6,4 +6,10 @@ module
 
     "http://gravatar.com/avatar/#{hash}?r=#{rating}&s=#{size}"
   end
+
+  def gravatar_image_tag(email, options = {})
+    classes = options.delete(:class)
+
+    image_tag gravatar_url(email, options), class: classes
+  end
 end
