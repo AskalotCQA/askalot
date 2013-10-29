17bb6e0f82d114be1171fca232e8c9502165ce73
Rastislav Dobšovič <rastislav.dobsovic@gmail.com>
2013-10-29 19:42:42 +0100

    Prepare migration and model for Question

---
+++ db/migrate/20131029183142_create_questions.rb
@@ -0,0 +1,15 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.string :title,        null: false
+      t.text :text,           null: false
+      t.references :user,     null: false
+      t.references :category, null: false
+      t.timestamps
+    end
+
+    add_index :questions, :title
+    add_index :questions, :user_id
+    add_index :questions, :category_id
+  end
+end
