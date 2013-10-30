31e11c6933a02e8aea9c0aeb3f4a7250ee8f8444
Pavol Zbell <pavol.zbell@gmail.com>
2013-10-30 08:11:03 +0100

    Fix relations between Question and Tag models

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,8 @@ class Question < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
 
+  has_many :question_taggings
+
   has_many :tags, through: :question_taggings
 
   validates :title, presence: true
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
+  has_many :question_taggings
+
   has_many :questions, through: :question_taggings
 
   validates :name, presence: true, uniqueness: true
