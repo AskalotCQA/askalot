8bbed80fa733be6bb22daaee98f2a02b2ab983f5
Samuel Molnar <molnar.samuel@gmail.com>
2013-11-05 13:25:53 +0100

    Add spec for editing user basic profile

--- spec/features/user/profile_spec.rb
+++ spec/features/user/profile_spec.rb
@@ -16,14 +16,28 @@ describe 'User Profile' do
       expect(page).to have_content(user.login)
     end
 
-    it 'allows editing of user profile' do
+    it 'edits basic profile', js: true do
       visit root_path
 
-      click_link 'Profil'
+      click_link user.email
+      click_link 'Nastavenia'
 
-      expect(page).to have_content(user.login)
+      fill_in 'user_nick',  with: 'Nicky'
+      fill_in 'user_first', with: 'Nick'
+      fill_in 'user_last',  with: 'Nickmann'
+      fill_in 'user_about', with: 'Lorem Ipsum'
 
-      click_link 'Upraviť profil'
+      click_button 'Uložiť'
+
+      expect(page).to have_content('Úspešne ste aktualizovali Váš účet.')
+      expect(page.current_path).to eql(edit_user_registration_path)
+
+      user.reload
+
+      expect(user.nick).to  eql('Nicky')
+      expect(user.first).to eql('Nick')
+      expect(user.last).to  eql('Nickmann')
+      expect(user.about).to eql('Lorem Ipsum')
     end
   end
 end
