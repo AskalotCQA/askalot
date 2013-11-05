27df434d5f341ab43bddb0d4bf945603754cd9e7
Samuel Molnar <molnar.samuel@gmail.com>
2013-11-05 14:58:29 +0100

    Add specs for edit profile

--- spec/features/user/profile_spec.rb
+++ spec/features/user/profile_spec.rb
@@ -16,11 +16,41 @@ describe 'User Profile' do
       expect(page).to have_content(user.login)
     end
 
-    it 'edits basic profile', js: true do
+    it 'edits user account', js: true do
       visit root_path
 
       click_link user.email
       click_link 'Nastavenia'
+
+      click_link 'Nastavenia účtu'
+
+      fill_in 'user_email', with: 'nicky.nickmann@gmail.com'
+
+      click_button 'Uložiť'
+
+      expect(page).to have_content('Aktuálne heslo – je povinná položka')
+
+      click_link 'Nastavenia účtu'
+
+      fill_in 'user_email', with: 'nicky.nickmann@gmail.com'
+      fill_in 'user_password', with: 'new password'
+      fill_in 'user_password_confirmation', with: 'new password'
+      fill_in 'user_current_password', with: user.password
+
+      click_button 'Uložiť'
+
+      expect(page.current_path).to eql(edit_user_registration_path)
+      expect(page).to have_content('Váš účet bol úspešne aktualizovaný ale potrebujeme overiť Vašu novú e-mailovú adresu.')
+    end
+
+    it 'edits basic user profile', js: true do
+      visit root_path
+
+      click_link user.email
+      click_link 'Nastavenia'
+
+      # TODO (smolnar) Add invalid values for models validations and check if the page shows 
+      # errors
 
       fill_in 'user_nick',  with: 'Nicky'
       fill_in 'user_first', with: 'Nick'
@@ -32,12 +62,37 @@ describe 'User Profile' do
       expect(page).to have_content('Úspešne ste aktualizovali Váš účet.')
       expect(page.current_path).to eql(edit_user_registration_path)
 
-      user.reload
+      expect(page).to have_field('user_nick',  with: 'Nicky')
+      expect(page).to have_field('user_first', with: 'Nick')
+      expect(page).to have_field('user_last',  with: 'Nickmann')
+      expect(page).to have_field('user_about', with: 'Lorem Ipsum')
+    end
 
-      expect(user.nick).to  eql('Nicky')
-      expect(user.first).to eql('Nick')
-      expect(user.last).to  eql('Nickmann')
-      expect(user.about).to eql('Lorem Ipsum')
+    it 'edits user social links', js: true do
+      visit root_path
+
+      click_link user.email
+      click_link 'Nastavenia'
+
+      click_link 'Sociálne siete'
+
+      # TODO (smolnar) Add invalid values for models validations and check if the page shows
+      # errors
+
+      fill_in 'user_facebook', with: 'facebook.com/nicky.nickmann'
+      fill_in 'user_twitter',  with: 'twitter.com/nnickmann'
+      fill_in 'user_linkedin', with: 'linkedin.com/nick.nickmann.jr'
+
+      click_button 'Uložiť'
+
+      expect(page).to have_content('Úspešne ste aktualizovali Váš účet.')
+      expect(page.current_path).to eql(edit_user_registration_path)
+
+      click_link 'Sociálne siete'
+
+      expect(page).to have_field('user_facebook', with: 'facebook.com/nicky.nickmann')
+      expect(page).to have_field('user_twitter',  with: 'twitter.com/nnickmann')
+      expect(page).to have_field('user_linkedin', with: 'linkedin.com/nick.nickmann.jr')
     end
   end
 end
