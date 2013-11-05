b5d82687f40b48bc43090cdf1e954b637dc369c8
Dušan Poizl <nou.spiro@gmail.com>
2013-11-05 20:02:35 +0100

    Removed exception handling from Question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,8 @@
 class QuestionsController < ApplicationController
   before_filter :authenticate_user!
 
-  rescue_from ActiveRecord::RecordNotFound, with: :question_not_found
-
   def show
     @question = Question.find(params[:id])
   end
 
-  def question_not_found
-
-  end
 end
