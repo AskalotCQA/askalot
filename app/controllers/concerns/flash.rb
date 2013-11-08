9e24a025f41439ab8dfb5d5f36432b43adf66222
Samuel Molnar <molnar.samuel@gmail.com>
2013-11-08 23:32:30 +0100

    Add optional flash parameter

--- app/controllers/concerns/flash.rb
+++ app/controllers/concerns/flash.rb
@@ -1,5 +1,5 @@
 module Concerns::Flash
-  def flash_error_messages_for(resource)
+  def flash_error_messages_for(resource, flash: flash.now, **options)
     messages = resource.errors.full_messages
 
     flash[:error] = Array.wrap(flash[:error]) + messages unless messages.empty?
