577ce394896f7220e4d7bec552820b0dbd45ab40
Samuel Molnar <molnar.samuel@gmail.com>
2013-11-09 02:16:37 +0100

    Update feature spec for new question

--- spec/features/add_question_spec.rb
+++ spec/features/add_question_spec.rb
@@ -24,11 +24,25 @@ describe 'Add Question' do
 
       fill_in 'question_title', with: 'Lorem ipsum title?'
       fill_in 'question_text',  with: 'Lorem ipsum'
+
       select category.name, from: 'question_category_id'
+
+      fill_in 'question_tag_list', with: 'tag1 Tag2 tAg3'
 
       click_button 'Vložiť otázku'
 
       expect(page).to have_content('Vaša otázka bola úspešne vložená.')
+
+      # TODO (smolnar) remove! use content check for attributes
+
+      expect(Question).to have(1).record
+
+      question = Question.first
+
+      expect(question.title).to eql('Lorem ipsum title?')
+      expect(question.text).to eql('Lorem ipsum')
+      expect(question.category).to eql(category)
+      expect(question.tag_list.sort).to eql(['tag1', 'tag2', 'tag3'])
     end
   end
 end
