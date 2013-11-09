12f5f8f0b27f17da27f0d31d3e6876abe1bdf810
Samuel Molnar <molnar.samuel@gmail.com>
2013-11-09 02:39:15 +0100

    Fix tags suggestion

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,15 @@
 class TagsController < ApplicationController
+  # TODO (smolnar) 
+  # * remove AR relation, use elasticsearch
+  # * use pagination
+
   def suggest
     relation = ActsAsTaggableOn::Tagging.where(taggable_type: params[:type].camelcase, context: params[:context] || :tags)
 
-    tags = relation.joins(:tag).where('tags.name LIKE ?', "#{params[:q]}%").map { |e| e.tag }
+    tags = relation.joins(:tag).where('tags.name LIKE ?', "#{params[:q]}%").select('tags.name').limit(10).distinct
 
-    render json: tags.map { |e| { id: e.name, text: e.name }}
+    render json: {
+      results: tags.uniq.map { |e| { id: e.name, text: e.name }},
+    }
   end
 end
