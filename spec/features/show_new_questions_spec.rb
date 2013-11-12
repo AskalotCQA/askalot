5e289ac48fde7c66197222e105e5b671b337836b
Peter Palenik <peter.ppalenik@gmail.com>
2013-11-12 15:55:27 +0100

    Change locales for date

--- spec/features/show_new_questions_spec.rb
+++ spec/features/show_new_questions_spec.rb
@@ -2,18 +2,22 @@ require 'spec_helper'
 
 describe 'Show Questions' do
   let(:user) { create :user }
-  let(:question) {create :question}
+  let(:category) { create :category }
+  let(:tag) { tag :tag }
+  let(:question) {create :question, author: user, category: category}
 
   context 'with current user' do
     before :each do
       login_as user
     end
-    it 'Show new questions', js: true do
+    it 'Show new questions' do
       visit root_path
 
-      click_link 'Zobrazenie nových otázok'
+     click_link 'Zobrazenie nových otázok'
 
       expect(page).to have_content(question.title)
+      expect(page).to have_content(question.author)
+      expect(page).to have_content(question.category.name)
       end
     end
 end
