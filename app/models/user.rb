ae5535926c338bf6e55fe8c238dd3b262a5e041b
Jozef Harinek <j.harinek@gmail.com>
2013-11-14 17:41:06 +0100

    Fix user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,16 +10,17 @@ class User < ActiveRecord::Base
 
          authentication_keys: [:login]
 
+  # TODO (jharinek) gravatar_email - do not allow blank, but needs to be fixed
   # TODO (smolnar) check uniqueness value select in db
   validates :login, format: { with: /\A[A-Za-z0-9_]+\z/ }, presence: true, uniqueness: { case_sensitive: false }
-  validates :nick, format: { with: /\A[A-Za-z0-9_]+\z/ }, presence: true
+  validates :nick, format: { with: /\A[A-Za-z0-9_]+\z/ }, presence: true, uniqueness: { case_sensitive: false }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/}, presence: true, uniqueness: { case_sensitive: false }
-  validates :gravatar_email, format: { with: /\A\z|\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
-  validates :first, format: { with: /\A\z|\A[A-Z][a-z]*\z/ }
-  validates :last, format: { with: /\A\z|\A[A-Z][a-z]*\z/ }
-  validates :facebook, format: { with: /\A\z|\A(http:\/\/)?(www.)?facebook.com\/[A-Za-z._\-]+\z/ }
-  validates :twitter, format: { with: /\A\z|\A(http:\/\/)?(www.)?twitter.com\/[A-Za-z._\-]+\z/ }
-  validates :linkedin, format: { with: /\A\z|\A(http:\/\/)?(www.)?linkedin.com\/in\/[A-Za-z._\-]+\z/ }
+  validates :gravatar_email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }, allow_blank: true
+  validates :first, format: { with: /\A[A-Z][a-z]*\z/ }, allow_blank: true
+  validates :last, format: { with: /\A[A-Z][a-z]*\z/ }, allow_blank: true
+  validates :facebook, format: { with: /\A(http:\/\/)?(www.)?facebook.com\/[A-Za-z._\-]+\z/ }, allow_blank: true
+  validates :twitter, format: { with: /\A(http:\/\/)?(www.)?twitter.com\/[A-Za-z._\-]+\z/ }, allow_blank: true
+  validates :linkedin, format: { with: /\A(http:\/\/)?(www.)?linkedin.com\/in\/[A-Za-z._\-]+\z/ }, allow_blank: true
 
   def gravatar_email
     read_attribute(:gravatar_email) || email
