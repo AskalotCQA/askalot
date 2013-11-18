acdce3d633b61406bf12b036c5e434f9b0a175bf
Samuel Molnar <molnar.samuel@gmail.com>
2013-11-18 23:57:50 +0100

    Prepare barebone test for showing question

--- spec/features/show_question_spec.rb
+++ spec/features/show_question_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 describe 'Show Question' do
-  let(:question) { create :question }
+  let(:question) { create :question, title: 'PostgreSQL setup' }
 
-  it 'show existing questions', js:true do
-    visit question_path(:id => question.id)
-
-    expect(page).to have_content('Lorem ipsum')
+  before :each do
+    login_as question.author
   end
 
+  it 'shows new question' do
+    visit root_path
+
+    click_link 'Otázky'
+
+    click_link 'PostgreSQL setup'
+
+    expect(page).to have_content('PostgreSQL setup')
+    expect(page).to have_content(question.text)
+    expect(page).to have_content(question.author.nick)
+  end
 end
