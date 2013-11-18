d6473ea8b5cb8b87c9596513b8eb8f3e748a734e
Samuel Molnar <molnar.samuel@gmail.com>
2013-11-19 00:29:27 +0100

    Adjust spec for new added question

--- spec/features/show_question_spec.rb
+++ spec/features/show_question_spec.rb
@@ -1,23 +1,23 @@
 require 'spec_helper'
 
-describe 'Show Questions' do
-  let(:user) { create :user }
-  let(:category) { create :category }
-  let(:tag) { tag :tag }
-  let(:question) {create :question, author: user, category: category}
+describe 'Show Question' do
+  let(:question) { create :question, :with_tags, title: 'PostgreSQL setup' }
 
-  context 'with current user' do
-    before :each do
-      login_as user
-    end
-    it 'Show new questions' do
-      visit root_path
+  before :each do
+    login_as question.author
+  end
 
-     click_link 'Zobrazenie nových otázok'
+  it 'shows new question' do
+    visit root_path
 
-      expect(page).to have_content(question.title)
-      expect(page).to have_content(question.author)
-      expect(page).to have_content(question.category.name)
-      end
-    end
+    click_link 'Otázky'
+
+    click_link 'PostgreSQL setup'
+
+    expect(page).to have_content('PostgreSQL setup')
+    expect(page).to have_content(question.text)
+    expect(page).to have_content(question.author.nick)
+
+    question.tags.pluck(:name).each { |tag| expect(page).to have_content(tag) }
+  end
 end
