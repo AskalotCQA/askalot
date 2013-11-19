310928aba950ae48f85ebc40131a84ffa1cb9f80
Pavol Zbell <pavol.zbell@gmail.com>
2013-11-19 20:56:20 +0100

    Fix a bug in question creation time helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,5 @@
 module QuestionsHelper
   def question_creation_time(question, options = {})
-    tooltip_time_tag question.created_at, options.merge(format: :normal, placement: :right)
+    tooltip_time_tag question.created_at, { format: :normal, placement: :right }.merge(options)
   end
 end
