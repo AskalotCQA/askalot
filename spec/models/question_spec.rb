c9876d63e0532f62cd257ecd05c36bbace278d98
Samuel Molnar <molnar.samuel@gmail.com>
2013-11-20 23:58:42 +0100

    Update question spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -20,4 +20,19 @@ describe Question do
 
     expect(question).to be_valid
   end
+
+  describe '#favoured_by?' do
+    it 'checks the valid favourer' do
+      user     = create :user
+      question = create :question
+
+      create :favorite, question: question, user: user
+
+      expect(question).to be_favoured_by(user)
+
+      another_user = create :user
+
+      expect(question).not_to be_favoured_by(another_user)
+    end
+  end
 end
