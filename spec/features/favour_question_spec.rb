78ae7c164ff9658b2e6d99a11959fe4d97f75442
Samuel Molnar <molnar.samuel@gmail.com>
2013-11-21 12:03:02 +0100

    Add specs for Favour Question feature

--- spec/features/favour_question_spec.rb
+++ spec/features/favour_question_spec.rb
@@ -8,8 +8,8 @@ describe 'Favour Question' do
     login_as user
   end
 
-  context 'with current user' do
-    it 'favours question' do
+  context 'when user is not a favourer' do
+    it 'favours the question', js: true do
       visit root_path
 
       click_link 'Otázky'
@@ -17,7 +17,28 @@ describe 'Favour Question' do
 
       click_link 'favour_question'
 
-      expect(user.favoured_questions).to include(question)
+      wait_for_remote
+
+      expect(question).to be_favoured_by(user)
+    end
+  end
+
+  context 'when user already a favourer' do
+    before :each do
+      question.favour_by! user
+    end
+
+    it 'unfavours the question', js: true do
+      visit root_path
+
+      click_link 'Otázky'
+      click_link question.title
+
+      click_link 'favour_question'
+
+      wait_for_remote
+
+      expect(question).not_to be_favoured_by(user)
     end
   end
 end
