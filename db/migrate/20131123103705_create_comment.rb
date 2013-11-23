380106f349601144d590d05eb1a38137fc4ab1a9
MarekGrznar <marek.grznar@gmail.com>
2013-11-23 17:26:31 +0100

    Fix comment migration

--- db/migrate/20131123103705_create_comment.rb
+++ db/migrate/20131123103705_create_comment.rb
@@ -1,18 +1,15 @@
 class CreateComment < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.references :author_id,      null: false,
-      t.references :commentable_id, null: false,
+      t.references :author_id,   null: false
+      t.references :commentable, null: false, polymorphic: true
 
-      t.string :commentable_type
-
-      t.string :text, null: false
+      t.text :text, null: false
 
       t.timestamps
     end
 
     add_index :comments, :author_id
-    add_index :comments, :commentable_id
-
+    add_index :comments, [:commentable_id, :commentable_type]
   end
 end
