af7ae263aaa4540cbaecfdcef7217781b59a3814
Dušan Poizl <nou.spiro@gmail.com>
2013-11-24 18:52:24 +0100

    Migration for Votes

---
+++ db/migrate/20131124171012_create_vote.rb
@@ -0,0 +1,15 @@
+class CreateVote < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.references :voter,   null: false
+      t.references :votable, null: false, polymorphic: true
+
+      t.boolean :upvote, null: false, default: true
+
+      t.timestamps
+    end
+
+    add_index :votes, :voter_id
+    add_index :votes, [:votable_id, :votable_type]
+  end
+end
