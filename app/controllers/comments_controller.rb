fc2e4c2606b6440ce184c51fd6ad61155c81fe11
Rastislav Dobšovič <rastislav.dobsovic@gmail.com>
2013-11-25 23:48:44 +0100

    Add controller for a new comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def create
+    @commentable = find_commentable
+    @question = Question.find params[:question_id]
+    @author   = @question.author
+    @comment   = @question.comments.build(comment_params)
+
+    if comment.save
+      flash[:notice] = t('comment.create.success')
+
+      redirect_to question_path(@question)
+    else
+      flash_error_messages_for comment
+
+      render 'questions/show'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(author: current_user)
+  end
+
+  def find_commentable
+    params.each do |name, value|
+      if name =~ /(.+)_id$/
+        return $1.classify.constantize.find(value)
+      end
+    end
+  end
+end
