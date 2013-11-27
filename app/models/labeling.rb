24ff2b61e4ced287706804eaffe222fa4395b5fd
Pavol Zbell <pavol.zbell@gmail.com>
2013-11-27 03:28:09 +0100

    Add scopes to Labeling

--- app/models/labeling.rb
+++ app/models/labeling.rb
@@ -2,4 +2,8 @@ class Labeling < ActiveRecord::Base
   belongs_to :author, class_name: :User
   belongs_to :answer
   belongs_to :label
+
+  scope :by,   lambda { |user| where author: user }
+  scope :for,  lambda { |answer| where answer: answer }
+  scope :with, lambda { |label| label.is_a?(Label) ? where(label: label): joins(:label).where(labels: { value: label }) }
 end
--- app/views/questions/favor.js.coffee
+++ app/views/questions/favor.js.coffee
@@ -1,2 +1,2 @@
 $(document).ready ->
-  $('.question-favoring').replaceWith("<%= escape_javascript render('questions/favoring', question: @question) %>")
+  $('#question-favoring-<%= @question.id %>').replaceWith("<%= escape_javascript render('questions/favoring', question: @question) %>")
