4614737eb03f209fc6e7c46279d060dcfc3f1843
Pavol Zbell <pavol.zbell@gmail.com>
2013-12-06 12:54:16 +0100

    Refactor home page layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,7 @@
 class StaticPagesController < ApplicationController
+  def home
+    @new      = Question.order(created_at: :desc).limit(5)
+    @answered = Question.answered.order(updated_at: :desc).limit(5)
+    @favored  = Question.favored.order(created_at: :desc).limit(5)
+  end
 end
