50c538f31c257e026f7a53874e4aea2421738d36
Dušan Poizl <nou.spiro@gmail.com>
2013-12-06 07:14:25 +0100

    Remove duplicated implementation in Voting

--- app/controllers/concerns/voting.rb
+++ app/controllers/concerns/voting.rb
@@ -1,17 +1,24 @@
 module Voting
   extend ActiveSupport::Concern
+
   def voteup
-    @votable = controller_name.classify.constantize.find(params[:id])
-
-    @votable.toggle_voteup_by!(current_user)
-
-    render :vote
+    vote true
   end
 
   def votedown
+    vote false
+  end
+
+  private
+
+  def vote(voteup)
     @votable = controller_name.classify.constantize.find(params[:id])
 
-    @votable.toggle_votedown_by!(current_user)
+    if voteup
+      @votable.toggle_voteup_by!(current_user)
+    else
+      @votable.toggle_votedown_by!(current_user)
+    end
 
     render :vote
   end
