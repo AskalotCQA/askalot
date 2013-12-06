1012b0b65a914bec730f93813d681f1d6347b918
Pavol Zbell <pavol.zbell@gmail.com>
2013-12-06 11:12:17 +0100

    Refactor question scopes

--- app/models/question.rb
+++ app/models/question.rb
@@ -16,9 +16,8 @@ class Question < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 2, maximum: 250 }
   validates :text,  presence: true, length: { minimum: 2 }
 
-  # TODO (smolnar) resolve order for answers.created_at
   scope :answered,   lambda { joins(:answers).uniq }
-  # TODO (smolnar) create favored scope, order by favorites.size, created_at
+  scope :favored,    lambda { joins(:favorites).uniq }
   scope :favored_by, lambda { |user| joins(:favorites).where(favorites: { user: user }) }
 
   def labels
