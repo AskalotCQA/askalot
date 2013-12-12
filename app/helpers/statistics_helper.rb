9da337789413760c6a64a029adaf8c2b7bf5c987
Pavol Zbell <pavol.zbell@gmail.com>
2013-12-12 16:10:37 +0100

    Improve table cell rendering helper

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,11 +1,30 @@
 module StatisticsHelper
-  def statistical_cell(value, options = {})
-    options = options.reverse_merge(negative: :danger, zero: :'text-muted', positive: nil)
+  def statistical_cell(value, options = {}, &block)
     classes = Array.wrap options[:class]
 
-    classes << options.delete(:negative) if value < 0
-    classes << options.delete(:zero)     if value.zero?
-    classes << options.delete(:positive) if value > 0
+    unless block_given?
+      map = {
+        negative: options.delete(:negative) || :danger,
+        zero:     options.delete(:zero) || :'text-muted',
+        positive: options.delete(:positive)
+      }
+
+      classes << map[:negative] if value < 0
+      classes << map[:zero]     if value.zero?
+      classes << map[:positive] if value > 0
+      classes << "text-#{classes.last}" unless classes.last.to_s.start_with?('text')
+      classes << :'text-right'
+    else
+      data = block.call(value, options)
+
+      if data.is_a? Array
+        value, classes = *data
+      elsif data.is_a? Hash
+        value, classes = data[:value], Array.wrap(data[:class])
+      else
+        classes += Array.wrap data
+      end
+    end
 
     content_tag :td, value, options.merge(class: classes)
   end
