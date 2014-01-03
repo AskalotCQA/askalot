4a92cc3a1fc309a3463ea5d55e2dc6876f2e4ba7
Samuel Molnar <molnar.samuel@gmail.com>
2014-01-03 20:41:04 +0100

    Add specs for questions polling

---
+++ spec/features/poll_questions_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'Poll Questions' do
+  it 'refreshes the questions each five seconds' do
+    pending
+  end
+
+  it 'refreshes filtered questions' do
+    pending
+  end
+end
