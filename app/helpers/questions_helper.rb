9118ef1cfe44f45fa22027a34c32ed66c82054a8
Pavol Zbell <pavol.zbell@gmail.com>
2014-01-05 22:43:24 +0100

    Remove comments from questions helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -33,9 +33,6 @@ module QuestionsHelper
 
     options.deep_merge! class: classes, data: { id: filter }
 
-    # TODO (smolnar) using only tags as parameter is possibly dangerous, review
-    # TODO (zbell) why dangerous?
-    # TODO (smolnar) omitting of other params, like page, tab and so on.
     link_to "#{label.name} (#{label.count})", questions_path(tags: filter), options
   end
 
