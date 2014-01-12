3fba18400598f9227b747b1ebeedf5cc5ad0fca0
Samuel Molnar <molnar.samuel@gmail.com>
2014-01-12 11:26:25 +0100

    Refactor current_params helper

--- spec/support/page_helper.rb
+++ spec/support/page_helper.rb
@@ -1,21 +1,11 @@
 module PageHelper
   def current_params
-    params = {}
+    params = Hash.new
+    uri    = URI.parse(current_url)
+    query  = CGI.parse(uri.query)
 
-    # TODO (smolnar) use rails api to parse params
-    URI.parse(current_url).query.split(/&/).each do |param|
-      key, value = param.split('=')
-
-      value = URI.decode(value)
-
-      if key =~ /\A.*\[\]\z/
-        key = key.sub(/\[\]/, '').to_sym
-
-        params[key] ||= Array.new
-        params[key] << value
-      else
-        params[key.to_sym] = value
-      end
+    query.each do |key, value|
+      params[key.to_sym] = value.first
     end
 
     params
