43a2113f4b53d3cc1579365bb156159b5e8bf2e4
MarekGrznar <marek.grznar@gmail.com>
2014-01-14 15:44:01 +0100

    Add test of add new comment

---
+++ spec/features/add_comment_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'Add Comment' do
+  let(:user) { create :user }
+  let!(:question) { create :question, :with_tags }
+
+  before :each do
+    login_as user
+  end
+
+  it 'adds new comment', js: true do
+    visit root_path
+
+    click_link 'Otázky'
+    click_link question.title
+
+    click_link 'Pridať komentár'
+
+    click_button 'Komentovať'
+
+    expect(page).to have_content('Komentár – je povinná položka')
+
+    click_link 'Pridať komentár'
+
+    fill_in 'comment[text]', with: 'My comment'
+
+    click_button 'Komentovať'
+
+    expect(page).to have_content('Váš komentár bol úspešne pridaný.')
+  end
+end
