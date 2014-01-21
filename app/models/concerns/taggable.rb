208273bdddd31276dd435cbf8352c0419920e56e
Samuel Molnar <molnar.samuel@gmail.com>
2014-01-21 17:46:49 +0100

    Add Taggable concern with specs

---
+++ app/models/concerns/taggable.rb
@@ -0,0 +1,76 @@
+module Taggable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :taggings, as: :taggable
+    has_many :tags, through: :taggings
+
+    scope :tagged_with, lambda { |values| joins(:tags).where tags: { name: Taggable::TagList.new(taggable, values).tags } }
+
+    after_save :generate_tags!
+  end
+
+  def tag_list
+    @tag_list ||= TagList.new(self.class.taggable)
+  end
+
+  def tag_list=(values)
+    @tag_list = TagList.new(self.class.taggable, values)
+  end
+
+  private
+
+  def generate_tags!
+    tag_list.each do |name|
+      tag = Tag.find_or_create_by! name: name
+
+      Tagging.find_or_create_by! tag_id: tag.id, taggable_id: self.id, taggable_type: self.class.base_class.name
+    end
+  end
+
+  module ClassMethods
+    def taggable
+      @taggable ||= Class.new do
+        include Squire::Base
+
+        config.extractor = TagList::Extractor
+      end
+    end
+  end
+
+  class TagList
+    include Enumerable
+
+    attr_reader   :values
+    attr_accessor :base, :extractor # TODO (smolnar) consider renaming base to configuration
+
+    def initialize(base, values = [])
+      @base   = base
+      @values = values
+    end
+
+    def extractor
+      @extractor ||= base.extractor? ? base.extractor : TagList::Extractor
+    end
+
+    def each
+      tags.each { |value| yield value }
+    end
+
+    def tags
+      @tags ||= extractor.extract(values)
+    end
+
+    def +(values)
+      @tags = tags + extractor.extract(values)
+    end
+
+    private
+
+    class Extractor
+      def self.extract(values)
+        (values.is_a?(Array) ? values.map(&:to_s) : values.split(/,/)).map(&:strip)
+      end
+    end
+  end
+end
