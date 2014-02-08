f3d5befbbdc7fbdd6a1e4eb17c900d3155a895e4
Samuel Molnar <molnar.samuel@gmail.com>
2014-02-08 13:16:34 +0100

    Finish unicorn setup

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,11 +2,9 @@ set :domain, '37.205.9.136'
 
 server domain, :app, :web, :db, primary: true
 
-set :deploy_to, "/home/deploy/projects/#{application}-staging"
 set :user,      'deploy'
 set :rails_env, 'staging'
 set :branch,    'staging'
+set :deploy_to, "/home/deploy/projects/#{application}-#{rails_env}"
 
 role :db, domain, primary: true
-
-set :rvm_ruby_string, "2.0.0@#{application}-#{rails_env}"
