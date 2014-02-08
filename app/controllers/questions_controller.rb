4291bc3b5c5af8eca45982cd49a515ef49540ff5
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-09 00:58:08 +0100

    Add missing ordering in question tabs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,7 +9,7 @@ class QuestionsController < ApplicationController
   def index
     @questions = case params[:tab].to_sym
                  when :'questions-new'        then Question.order(created_at: :desc)
-                 when :'questions-unanswered' then Question.unanswered
+                 when :'questions-unanswered' then Question.unanswered.order(created_at: :desc)
                  when :'questions-answered'   then Question.answered.order(created_at: :desc)
                  when :'questions-solved'     then Question.solved.order(created_at: :desc)
                  when :'questions-favored'    then Question.favored.order(created_at: :desc)
