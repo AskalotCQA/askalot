72bdf6b5ed4348d7f2ea12fe4d4095e8b4948191
Samuel Molnar <molnar.samuel@gmail.com>
2014-02-08 12:38:34 +0100

    Fix unicorn paths

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,12 +1,12 @@
-root = Rails.root
+root = File.expand_path "#{File.dirname(__FILE__)}/.."
 
 worker_processes 8
 timeout          15
 preload_app      true
-listen           root.join('shared/.unicorn.sock'), backlog: 64
-pid              root.join('tmp/pids/unicorn.pid')
-stderr_path      root.join('log/unicorn.log')
-stdout_path      root.join('log/unicorn.log')
+listen           "#{root}/shared/.unicorn.sock", backlog: 64
+pid              "#{root}/tmp/pids/unicorn.pid"
+stderr_path      "#{root}/log/unicorn.log"
+stdout_path      "#{root}/log/unicorn.log"
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
