91acec669371d79069929fde7faf133fd53ec5cd
Peter Palenik <peter.ppalenik@gmail.com>
2014-02-08 16:59:08 +0100

    Fix tab for unanswered questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -18,8 +18,9 @@ class Question < ActiveRecord::Base
   validates :text,      presence: true, length: { minimum: 2 }
   #validates :anonymous, presence: true #TODO(zbell) Rasto: uncomment this when tests do not fail because of it
 
-  scope :answered, lambda { joins(:answers).uniq }
-  scope :by,       lambda { |user| where(author: user) }
+  scope :answered,    lambda { joins(:answers).uniq }
+  scope :unanswered,  lambda { includes(:answers).where(answers: {question_id: nil}) }
+  scope :by,          lambda { |user| where(author: user) }
 
   def labels
     [category] + tags_with_counts
