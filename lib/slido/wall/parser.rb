b8c2064f276d32d8fece96751fda1a342b00cc4a
Samuel Molnar <molnar.samuel@gmail.com>
2014-02-10 08:37:11 +0100

    Add additional data for Slido wall parser

--- lib/slido/wall/parser.rb
+++ lib/slido/wall/parser.rb
@@ -36,9 +36,11 @@ module Slido::Wall
 
       data = JSON.parse(json, symbolize_names: true)
 
+      result.uuid      = data[:event_id]
       result.name      = data[:name]
       result.starts_at = Time.parse(data[:date_from])
       result.ends_at   = Time.parse(data[:date_to])
+      result.url       = "https://#{document.css('#wall .major span.link').text}"
 
       result
 >>>>>>> Add Slido wall parser
