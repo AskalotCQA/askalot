f23eeec6e457fc0397dd19ed609088bcbffe2bb3
Samuel Molnar <molnar.samuel@gmail.com>
2014-02-10 08:22:10 +0100

    Add Slido wall parser

--- lib/slido/wall/parser.rb
+++ lib/slido/wall/parser.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module Slido
   module Wall
     class Parser
@@ -20,6 +21,27 @@ module Slido
 
         result
       end
+=======
+require 'ostruct'
+
+module Slido::Wall
+  class Parser
+    def self.parse(html)
+      document = Nokogiri::HTML(html)
+      result   = OpenStruct.new
+
+      context = document.css('.wall-base #content script:first').text
+
+      _, json = *context.match(/event:\s+(?<json>.*}),\s+cols:/)
+
+      data = JSON.parse(json, symbolize_names: true)
+
+      result.name      = data[:name]
+      result.starts_at = Time.parse(data[:date_from])
+      result.ends_at   = Time.parse(data[:date_to])
+
+      result
+>>>>>>> Add Slido wall parser
     end
   end
 end
