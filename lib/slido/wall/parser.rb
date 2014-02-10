77e68d431b0abc69586e42ddf9cf8971ae7a2f2e
Samuel Molnar <molnar.samuel@gmail.com>
2014-02-10 09:46:22 +0100

    Add Slido scraper and few utilities into Gemfile

--- lib/slido/wall/parser.rb
+++ lib/slido/wall/parser.rb
@@ -1,23 +1,25 @@
-module Slido::Wall
-  class Parser
-    def self.parse(html)
-      document = Nokogiri::HTML(html)
-      result   = OpenStruct.new
+module Slido
+  module Wall
+    class Parser
+      def self.parse(html)
+        document = Nokogiri::HTML(html)
+        result   = OpenStruct.new
 
-      context = document.css('.wall-base #content script:first').text
+        context = document.css('.wall-base #content script:first').text
 
-      _, json = *context.match(/event:\s+(?<json>.*}),\s+cols:/)
+        _, json = *context.match(/event:\s+(?<json>.*}),\s+cols:/)
 
-      data = JSON.parse(json, symbolize_names: true)
+        data = JSON.parse(json, symbolize_names: true)
 
-      result.uuid       = data[:event_id].to_i
-      result.identifier = data[:hash]
-      result.name       = data[:name]
-      result.starts_at  = Time.parse(data[:date_from])
-      result.ends_at    = Time.parse(data[:date_to])
-      result.url        = "https://www.sli.do/#{result.identifier}"
+        result.uuid       = data[:event_id].to_i
+        result.identifier = data[:hash]
+        result.name       = data[:name]
+        result.starts_at  = Time.parse(data[:date_from])
+        result.ends_at    = Time.parse(data[:date_to])
+        result.url        = "#{Slido.base}/#{result.identifier}"
 
-      result
+        result
+      end
     end
   end
 end
