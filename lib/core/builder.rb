0a920d7acc46cb8ac0c1ca164922fc94f7b7bd17
Samuel Molnar <molnar.samuel@gmail.com>
2014-02-10 16:35:43 +0100

    Add core utilities

---
+++ lib/core/builder.rb
@@ -0,0 +1,28 @@
+module Core
+  module Builder
+    extend self
+
+    def method_missing(method, *args, &block)
+      _, model_name = *method.to_s.match(/\Acreate_(?<model>\w+)_by\z/)
+
+      if model_name
+        model_name = model_name.classify
+        model      = model_name.constantize rescue nil
+
+        raise ArgumentError.new("Cannot find model `#{model_name}`.") unless model
+
+        keys       = args[0..-2]
+        attributes = args.last
+        conditions = attributes.slice(*keys)
+
+        record = model.find_or_initialize_by(conditions)
+
+        record.update_attributes!(attributes)
+
+        record
+      else
+        super(method, *args, &block)
+      end
+    end
+  end
+end
