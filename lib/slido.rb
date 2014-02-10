f682cc35e7667e0ca44ab92703c50b2d6619869a
Samuel Molnar <molnar.samuel@gmail.com>
2014-02-10 07:58:51 +0100

    Add Slido questions parser

--- lib/slido.rb
+++ lib/slido.rb
@@ -1,10 +1,1 @@
 require 'slido/questions/parser'
-require 'slido/wall/parser'
-require 'slido/scraper'
-
-# TODO (smolnar) use Squire, resolve why fails in stub_const
-module Slido
-  def self.base
-    'https://www.sli.do'
-  end
-end
