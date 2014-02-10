6739f0ad2cac2f8206a7b64aaec97c97c7f2daef
Samuel Molnar <molnar.samuel@gmail.com>
2014-02-10 07:58:51 +0100

    Add Slido questions parser

---
+++ lib/slido.rb
@@ -0,0 +1,1 @@
+require 'slido/questions/parser'
---
+++ lib/slido/questions/parser.rb
@@ -0,0 +1,21 @@
+require 'ostruct'
+
+module Slido
+  module Questions
+    class Parser
+      def self.parse(json)
+        data = JSON.parse(json, symbolize_names: true)
+
+        data.map do |question|
+          result = OpenStruct.new
+
+          result.title    = question[:text]
+          result.text     = question[:text]
+          result.tag_list = :slido
+
+          result
+        end
+      end
+    end
+  end
+end
