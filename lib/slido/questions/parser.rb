b1933288254276f7c20f932313a9bf375b83a0fb
Samuel Molnar <molnar.samuel@gmail.com>
2014-02-10 08:22:10 +0100

    Add Slido wall parser

--- lib/slido/questions/parser.rb
+++ lib/slido/questions/parser.rb
@@ -1,31 +1,18 @@
-<<<<<<< HEAD
-=======
 require 'ostruct'
 
->>>>>>> Add Slido questions parser
-module Slido
-  module Questions
-    class Parser
-      def self.parse(json)
-        data = JSON.parse(json, symbolize_names: true)
+module Slido::Questions
+  class Parser
+    def self.parse(json)
+      data = JSON.parse(json, symbolize_names: true)
 
-        data.map do |question|
-          result = OpenStruct.new
+      data.map do |question|
+        result = OpenStruct.new
 
-<<<<<<< HEAD
-          result.title            = question[:text]
-          result.text             = question[:text]
-          result.slido_event_uuid = question[:event_id].to_i
-          result.slido_uuid       = question[:event_question_id].to_i
-          result.tag_list         = :slido
-=======
-          result.title    = question[:text]
-          result.text     = question[:text]
-          result.tag_list = :slido
->>>>>>> Add Slido questions parser
+        result.title    = question[:text]
+        result.text     = question[:text]
+        result.tag_list = :slido
 
-          result
-        end
+        result
       end
     end
   end
