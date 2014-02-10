c96b917de9632a2396ade94530c8d37aeb5b0877
Samuel Molnar <molnar.samuel@gmail.com>
2014-02-10 17:14:12 +0100

    Add missing indices of Slido attributes

--- db/migrate/20140210161235_add_indices_for_slido_attributes.rb
+++ db/migrate/20140210161235_add_indices_for_slido_attributes.rb
@@ -1,9 +1,7 @@
 class AddIndicesForSlidoAttributes < ActiveRecord::Migration
   def change
-    add_index :categories, :slido_username
-
-    add_index :questions, :slido_uuid, unique: true
-
-    add_index :slido_events, :uuid, unique: true
+    add_index :categories,   :slido_username
+    add_index :slido_events, :uuid,       unique: true
+    add_index :questions,    :slido_uuid, unique: true
   end
 end
