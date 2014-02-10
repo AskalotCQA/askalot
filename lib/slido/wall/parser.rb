9300c957992ecb82971b70b33cb4ff61d530e799
Samuel Molnar <molnar.samuel@gmail.com>
2014-02-10 08:49:02 +0100

    Fix Slido identifiers

--- lib/slido/wall/parser.rb
+++ lib/slido/wall/parser.rb
@@ -10,11 +10,12 @@ module Slido::Wall
 
       data = JSON.parse(json, symbolize_names: true)
 
-      result.uuid      = data[:hash]
-      result.name      = data[:name]
-      result.starts_at = Time.parse(data[:date_from])
-      result.ends_at   = Time.parse(data[:date_to])
-      result.url       = "https://#{document.css('#wall .major span.link').text}"
+      result.uuid       = data[:event_id].to_i
+      result.identifier = data[:hash]
+      result.name       = data[:name]
+      result.starts_at  = Time.parse(data[:date_from])
+      result.ends_at    = Time.parse(data[:date_to])
+      result.url        = "https://www.sli.do/#{result.identifier}"
 
       result
     end
