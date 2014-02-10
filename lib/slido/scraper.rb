fe25023fbeb42da8d4208f49eb7933a763f597ed
Samuel Molnar <molnar.samuel@gmail.com>
2014-02-10 09:46:22 +0100

    Add Slido scraper and few utilities into Gemfile

--- lib/slido/scraper.rb
+++ lib/slido/scraper.rb
@@ -1,8 +1,6 @@
 module Slido
   class Scraper
-    def self.run(category, options = {})
-      username = category.slido_username
-
+    def self.run(username, options = {})
       uri = "#{Slido.base}/#{username}"
 
       response = Scout::Downloader.download(uri, with_response: true)
@@ -15,18 +13,14 @@ module Slido
       content = Scout::Downloader.download("#{uri}/wall")
       event   = Slido::Wall::Parser.parse(content)
 
-      attributes = event.to_h.merge(category_id: category.id)
+      category = Category.find_by slido_username: username
 
-      event = Core::Builder.create_slido_event_by(:uuid, attributes)
-
-      event.update_attributes!(attributes)
+      SlidoEvent.create!(event.to_h)
 
       questions.each do |question|
         attributes = question.to_h.merge(category_id: category.id)
 
-        question = Core::Builder.create_question_by(:slido_uuid, attributes)
-
-        question.update_attributes!(attributes)
+        Question.create!(attributes)
       end
     end
   end
