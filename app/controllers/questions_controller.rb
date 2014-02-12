41260125d4d8cbc87a7670edaa098d68677ae6e6
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-12 11:52:14 +0100

    Fix favored question ordering

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,7 +12,7 @@ class QuestionsController < ApplicationController
                  when :'questions-unanswered' then Question.unanswered#.order(votes_total: :desc, created_at: :desc) #TODO(zbell) order
                  when :'questions-answered'   then Question.answered.order(votes_total: :desc, created_at: :desc)
                  when :'questions-solved'     then Question.solved.order(votes_total: :desc, created_at: :desc)
-                 when :'questions-favored'    then Question.favored.order(created_at: :desc) #TODO(zbell) order by favorites.count
+                 when :'questions-favored'    then Question.favored.order(favorites_count: :desc, created_at: :desc)
                  else fail
                  end
 
