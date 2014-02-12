601baca337dc3152cf76fe8b8e2eb160b725d96b
MarekGrznar <marek.grznar@gmail.com>
2014-02-13 00:52:13 +0100

    Add validator on evaluation value

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -2,5 +2,7 @@ class Evaluation < ActiveRecord::Base
   belongs_to :evaluator, class_name: :User
   belongs_to :evaluable, polymorphic: true
 
+  validates :value, presence: true, inclusion: { in: -2..2 }
+
   scope :by, lambda { |user| where(evaluator: user) }
 end
