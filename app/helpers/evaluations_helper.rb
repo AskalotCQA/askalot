480a278de7aaa372d3759409657a939471b778d8
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-15 21:12:23 +0100

    Improve evaluations helper

--- app/helpers/evaluations_helper.rb
+++ app/helpers/evaluations_helper.rb
@@ -3,9 +3,10 @@ module EvaluationsHelper
   Boundary = 2.0 / 3.0
 
   def evaluation_badge_tag(evaluable, options = {})
-    data = evaluation_data evaluable
+    data  = evaluation_data evaluable
+    title = translate "evaluation.rank.#{evaluable.class.to_s.downcase}.#{data[:rank]}"
 
-    content_tag :span, options.merge(class: :'fa-stack') do
+    content_tag :span, tooltip_attributes(title, options.merge(class: :'evaluated fa-stack', placement: :bottom)) do
       icon_tag(:circle, class: [:'fa-stack-2x', data[:color]]) + icon_tag(data[:icon], class: [:'fa-stack-1x', :'text-inverse'])
     end
   end
@@ -20,9 +21,9 @@ module EvaluationsHelper
 
   def evaluation_data(evaluable)
     case evaluable.evaluations.average(:value).to_f
-    when -Infinity...-Boundary then { color: :'text-danger',  icon: :'thumbs-o-down' }
-    when -Boundary...+Boundary then { color: :'text-muted',   icon: :'hand-o-right' }
-    when +Boundary.. +Infinity then { color: :'text-success', icon: :'thumbs-o-up' }
+    when -Infinity...-Boundary then { color: :'evaluated-bad',     icon: :'thumbs-o-down', rank: :bad }
+    when -Boundary...+Boundary then { color: :'evaluated-neutral', icon: :'hand-o-right',  rank: :neutral }
+    when +Boundary.. +Infinity then { color: :'evaluated-good',    icon: :'thumbs-o-up',   rank: :good }
     else fail
     end
   end
