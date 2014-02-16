a437128168ec22272710cf91bcf997b221892572
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-16 14:25:32 +0100

    Rename slido_uuid to slido_question_uuid in Question

--- lib/slido/questions/parser.rb
+++ lib/slido/questions/parser.rb
@@ -7,11 +7,11 @@ module Slido
         data.map do |question|
           result = OpenStruct.new
 
-          result.title            = question[:text]
-          result.text             = question[:text]
-          result.slido_event_uuid = question[:event_id].to_i
-          result.slido_uuid       = question[:event_question_id].to_i
-          result.tag_list         = :slido
+          result.title               = question[:text]
+          result.text                = question[:text]
+          result.slido_event_uuid    = question[:event_id].to_i
+          result.slido_question_uuid = question[:event_question_id].to_i
+          result.tag_list            = :slido
 
           result
         end
