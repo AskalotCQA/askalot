804c2036bd9d5e297bace0345da644323e0dc585
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-16 23:49:23 +0100

    Fix production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ NaRuby::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'TODO' }
+  config.action_mailer.default_url_options = { host: 'askalot.fiit.stuba.sk' }
 
   # Precompilation for I18n-js
   config.assets.initialize_on_precompile = true
