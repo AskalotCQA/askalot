a7b420da4860f148877f4a1664046d00428884fb
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-18 23:52:36 +0100

    Implement user filtering by question tags for statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -4,6 +4,26 @@ class StatisticsController < ApplicationController
   def index
     authorize! :observe, nil
 
-    @users = User.order(:name).all
+    @questions = Question
+    @users     = User.order(:name)
+
+    filter_by_tags
+  end
+
+  private
+
+  def filter_by_tags
+    return unless params[:tags].present?
+
+    @questions = @questions.tagged_with(params[:tags])
+
+    authors                = Question.tagged_with(params[:tags]).uniq.pluck(:author_id)
+    answer_authors         = Answer.joins(:question).where(question_id: @questions).uniq.pluck(:author_id)
+    comment_authors        = Comment.where(commentable_type: :Question).joins('INNER JOIN questions ON questions.id = commentable_id').where(commentable_id: @questions).uniq.pluck(:author_id)
+    answer_comment_authors = Comment.where(commentable_type: :Answer).joins('INNER JOIN answers ON answers.id = commentable_id INNER JOIN questions ON questions.id = answers.question_id').where(questions: { id: @questions }).uniq.pluck(:author_id)
+    voters                 = Vote.where(votable_type: :Question).joins('INNER JOIN questions ON questions.id = votable_id').where(votable_id: @questions).uniq.pluck(:voter_id)
+    answer_voters          = Vote.where(votable_type: :Answer).joins('INNER JOIN answers ON answers.id = votable_id INNER JOIN questions ON questions.id = answers.question_id').where(questions: { id: @questions }).uniq.pluck(:voter_id)
+
+    @users = @users.where(id: (authors.to_set + answer_authors + comment_authors + answer_comment_authors + voters + answer_voters).to_a)
   end
 end
