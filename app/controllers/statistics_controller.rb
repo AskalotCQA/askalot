23984be9676451a4df927557330a1d6db639b6a2
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-19 15:49:55 +0100

    Partially fix statistics computation

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -19,10 +19,10 @@ class StatisticsController < ApplicationController
 
     authors                = Question.tagged_with(params[:tags]).uniq.pluck(:author_id)
     answer_authors         = Answer.joins(:question).where(question_id: @questions).uniq.pluck(:author_id)
-    comment_authors        = Comment.where(commentable_type: :Question).joins('INNER JOIN questions ON questions.id = commentable_id').where(commentable_id: @questions).uniq.pluck(:author_id)
-    answer_comment_authors = Comment.where(commentable_type: :Answer).joins('INNER JOIN answers ON answers.id = commentable_id INNER JOIN questions ON questions.id = answers.question_id').where(questions: { id: @questions }).uniq.pluck(:author_id)
-    voters                 = Vote.where(votable_type: :Question).joins('INNER JOIN questions ON questions.id = votable_id').where(votable_id: @questions).uniq.pluck(:voter_id)
-    answer_voters          = Vote.where(votable_type: :Answer).joins('INNER JOIN answers ON answers.id = votable_id INNER JOIN questions ON questions.id = answers.question_id').where(questions: { id: @questions }).uniq.pluck(:voter_id)
+    comment_authors        = Comment.for(Question).joins('INNER JOIN questions ON questions.id = commentable_id').where(commentable_id: @questions).uniq.pluck(:author_id)
+    answer_comment_authors = Comment.for(Answer).joins('INNER JOIN answers ON answers.id = commentable_id INNER JOIN questions ON questions.id = answers.question_id').where(questions: { id: @questions }).uniq.pluck(:author_id)
+    voters                 = Vote.for(Question).joins('INNER JOIN questions ON questions.id = votable_id').where(votable_id: @questions).uniq.pluck(:voter_id)
+    answer_voters          = Vote.for(Answer).joins('INNER JOIN answers ON answers.id = votable_id INNER JOIN questions ON questions.id = answers.question_id').where(questions: { id: @questions }).uniq.pluck(:voter_id)
 
     @users = @users.where(id: (authors.to_set + answer_authors + comment_authors + answer_comment_authors + voters + answer_voters).to_a)
   end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -12,8 +12,8 @@ class Answer < ActiveRecord::Base
 
   validates :text, presence: true
 
-  scope :by,   lambda { |user| where(author: user) }
-  scope :for,  lambda { |question| where(question: question) }
+  scope :by,  lambda { |user| where(author: user) }
+  scope :for, lambda { |question| where(question: question) }
 
   scope :labeled_with, lambda { |label| joins(:labelings).merge(Labeling.with label) }
 
