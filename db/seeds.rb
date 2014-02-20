d1acaa428484da410577b5910c03948f0dc690a3
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-20 11:12:39 +0100

    Update sli.do user about text

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ slido.update_attributes(
   password: SecureRandom.hex,
   first: 'Automaton',
   last: 'Slido',
-  about: 'Som automatický robot, ktorý zbiera otázky zo systému sli.do',
+  about: 'Automatizovaný robot, ktorý zbiera otázky zo systému sli.do',
   show_email: false,
   role: :student
 )
