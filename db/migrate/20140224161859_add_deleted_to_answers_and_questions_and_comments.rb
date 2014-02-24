861466f67ee14a223e83b00c86602823a8741da9
MarekGrznar <marek.grznar@gmail.com>
2014-02-24 17:22:02 +0100

    Add migration to adding deleted flag to qac

---
+++ db/migrate/20140224161859_add_deleted_to_answers_and_questions_and_comments.rb
@@ -0,0 +1,7 @@
+class AddDeletedToAnswersAndQuestionsAndComments < ActiveRecord::Migration
+  def change
+    add_column :answers,   :deleted, :boolean, null: false, default: false
+    add_column :questions, :deleted, :boolean, null: false, default: false
+    add_column :comments,  :deleted, :boolean, null: false, default: false
+  end
+end
