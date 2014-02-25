6d17be4f626d16a43e741da94e9746d996cf67ec
Samuel Molnar <molnar.samuel@gmail.com>
2014-02-25 15:51:30 +0100

    Respect deleted scope for taggable models

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,11 +1,15 @@
 class Tag < ActiveRecord::Base
   include Watchable
 
-  attr_accessor :count
-
   has_many :taggings, dependent: :restrict_with_exception
 
   before_save :normalize
+
+  def count
+    @count ||= taggings.select do |tagging|
+      tagging.taggable.respond_to?(:deleted) ? !tagging.taggable.deleted : true
+    end.size
+  end
 
   def normalize
     self.name = name.to_s.downcase.gsub(/[^[:alnum:]]+/, '-').gsub(/\A-|-\z/, '')
