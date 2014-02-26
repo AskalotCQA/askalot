3973b3572729b6525af2a6563d37984c8c61cf58
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-26 13:12:15 +0100

    Add migration for Notifications

--- db/migrate/20140226035359_create_notifications.rb
+++ db/migrate/20140226035359_create_notifications.rb
@@ -1,8 +1,23 @@
 class CreateNotifications < ActiveRecord::Migration
   def change
     create_table :notifications do |t|
+      t.references :recipient,  null: false
+      t.references :initiator,  null: false
+      t.references :notifiable, null: false, polymorphic: true
+
+      t.string  :action, null: false
+      t.boolean :unread, null: false, default: true
 
       t.timestamps
     end
+
+    add_index :notifications, :recipient_id
+    add_index :notifications, :initiator_id
+    add_index :notifications, [:notifiable_id, :notifiable_type]
+
+    add_index :notifications, :action
+    add_index :notifications, :unread
+
+    add_index :notifications, :created_at
   end
 end
