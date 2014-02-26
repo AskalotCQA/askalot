d44074816625001ff0493c436249d680e2070ba6
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-26 04:42:25 +0100

    Completely refactor statistics helpers

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,50 +1,68 @@
 module StatisticsHelper
-  def average(relation, column)
-    relation.average(column).to_f
+  def mean(values)
+    values.sum.to_f / values.count
   end
 
-  def median(relation, column)
-    return unless (size = relation.size).zero?
+  def median(values)
+    values, count = values.sort, values.count
 
-    relation.order(column).offset((size - 1) / 2).limit(2 - (size % 2)).sum(column) / 2.0
+    (values[(count - 1) / 2] + values[count / 2]) / 2.0
   end
 
-  def standard_deviation(relation, column)
-    ActiveRecord::Base.connection.select_value(relation.select("stddev(#{column})").to_sql).to_f
+  def mean_and_standard_deviation(values)
+    mean = mean(values)
+    sum  = values.inject(0) { |a, i| a + (i - mean) ** 2 }
+
+    return mean, Math.sqrt(sum / (values.count - 1)).to_f
   end
 
-  def statistical_fraction(numeration, denomination)
-    content_tag(:td, numeration.size, class: :'text-right') +
-    content_tag(:td, number_to_percentage(100 * numeration.size / denomination.size.to_f, precision: 2), class: :'text-right')
-  end
+  # TODO(zbell) rm
+  #def average2(relation, column)
+  #  relation.average(column).to_f
+  #end
+  #
+  #def median2(relation, column)
+  #  return unless (size = relation.size).zero?
+  #
+  #  relation.order(column).offset((size - 1) / 2).limit(2 - (size % 2)).sum(column) / 2.0
+  #end
+  #
+  #def standard_deviation2(relation, column)
+  #  ActiveRecord::Base.connection.select_value(relation.select("stddev(#{column})").to_sql).to_f
+  #end
 
-  def statistical_cell(value, options = {}, &block)
+  def data_tag(value, options = {})
     classes = Array.wrap options[:class]
 
-    unless block_given?
-      map = {
-        negative: options.delete(:negative) || :danger,
-        zero:     options.delete(:zero) || :'text-muted',
-        positive: options.delete(:positive)
-      }
-
-      classes << map[:negative] if value < 0
-      classes << map[:zero]     if value.zero?
-      classes << map[:positive] if value > 0
-      classes << "text-#{classes.last}" unless classes.last.blank? || classes.last.to_s.start_with?('text')
-      classes << :'text-right'
+    if block_given?
+      data    = yield value, options
+      value   = data.first
+      classes = Array.wrap(data.second)
     else
-      data = block.call(value, options)
-
-      if data.is_a? Array
-        value, classes = data.first, Array.wrap(data.second)
-      elsif data.is_a? Hash
-        value, classes = data[:value], Array.wrap(data[:class])
-      else
-        classes += Array.wrap data
-      end
+      classes << :'text-muted' if value.zero?
+      classes << :'text-right'
     end
 
     content_tag :td, value, options.merge(class: classes.reject(&:blank?))
+  end
+
+  def data_description_tag(relation, *chain)
+    if block_given?
+      values = relation.map { |o| yield o }
+    else
+      values = chain.any? ? relation.map { |o| chain.inject(o) { |r, m| r.send(m) }.size } : relation.to_a
+    end
+
+    median          = median(values)
+    mean, deviation = mean_and_standard_deviation(values)
+
+    content_tag(:td, number_with_precision(median, precision: 2), class: :'text-right') +
+    content_tag(:td, number_with_precision(mean, precision: 2), class: :'text-right') +
+    content_tag(:td, number_with_precision(deviation, precision: 2), class: :'text-right')
+  end
+
+  def data_fraction_tag(numeration, denomination)
+    content_tag(:td, numeration.size, class: :'text-right') +
+    content_tag(:td, number_to_percentage(100 * numeration.size / denomination.size.to_f, precision: 2), class: :'text-right')
   end
 end
