bf9b4481874769ec0c8d1f505900ba00505e22b1
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-26 04:55:50 +0100

    Add some notes to tests

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,6 +1,5 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :notification do
+    # TODO(zbell) implement
   end
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe Notification do
-  pending "add some examples to (or delete) #{__FILE__}"
+  pending "add some examples to #{__FILE__}"
 end
