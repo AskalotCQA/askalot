4ab02e0ef10bf1b707d9273c15c6681d3f395211
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-27 09:52:17 +0100

    Improve Markdown options dispatch

--- lib/redcurtain/markdown.rb
+++ lib/redcurtain/markdown.rb
@@ -8,18 +8,20 @@ module Redcurtain
       @renderers = [
         Redcurtain::Renderer::Gemoji,
         Redcurtain::Renderer::GitHub,
-        Redcurtain::Highlighter::Pygments
+        Redcurtain::Renderer::Pygments
       ]
     end
 
     def render(content, options = {})
+      options.symbolize_keys!
+
       renderers.inject(content) do |result, renderer|
-        renderer.render(result, options)
+        renderer.render(result, options[renderer.name.to_s.split(/::/).last.downcase.to_sym])
       end
     end
 
     def strip(content, options = {})
-      Nokogiri::XML(content).text
+      Nokogiri::XML(content).text.strip
     end
   end
 end
