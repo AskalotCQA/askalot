7c807731924b7d99edde9ddf9bec57fecd96a08c
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-27 09:52:36 +0100

    Refactor Markdown tests

--- spec/lib/redcurtain/markdown_spec.rb
+++ spec/lib/redcurtain/markdown_spec.rb
@@ -3,41 +3,50 @@ require 'spec_helper'
 describe Redcurtain::Markdown do
   let(:markdown) { described_class }
 
+  let(:options) do
+    {
+      gemoji: {
+        class: 'doge-class',
+        path: '/assets'
+      }
+    }
+  end
+
   after :each do
     Redcurtain::Markdown.setup!
   end
 
   describe '#render' do
+    it 'checks renderer chaining' do
+      o = { a: { key: 0 }, b: { key: 1 }, c: { key: 2 } }
+
+      a = double(:renderer, name: 'Renderer::A')
+      b = double(:renderer, name: 'Renderer::B')
+      c = double(:renderer, name: 'Renderer::C')
+
+      markdown.renderers = [a, b, c]
+
+      expect(a).to receive(:render).with('a', { key: 0 }).and_return('b')
+      expect(b).to receive(:render).with('b', { key: 1 }).and_return('c')
+      expect(c).to receive(:render).with('c', { key: 2 }).and_return('d')
+
+      expect(markdown.render('a', o)).to eql('d')
+    end
+
     it 'renders markdown' do
-      renderer    = double(:renderer)
-      highlighter = double(:highlighter)
+      text = "`code` :dog: ```ruby\ndoge.code```\nhello :unknown-doge:"
+      html = "<p><code>code</code> <img class=\"doge-class\" src=\"/assets/dog.png\" alt=\"dog\"><code>ruby\ndoge.code</code>\nhello :unknown-doge:</p>"
 
-      markdown.renderer    = renderer
-      markdown.highlighter = highlighter
-
-      html = "<pre lang=\"ruby\">puts 'Hello, world.'</pre>"
-
-      expect(renderer).to    receive(:render).with('text').and_return(html)
-      expect(highlighter).to receive(:highlight).with("puts 'Hello, world.'", language: :ruby).and_return('<pre>highlighted code</pre>')
-
-      expect(markdown.render('text')).to include('<pre>highlighted code</pre>')
+      expect(markdown.render(text, options)).to include(html)
     end
   end
 
   describe '#strip' do
     it 'renders plain text from markdown' do
-      renderer    = double(:renderer)
-      highlighter = double(:highlighter)
+      html = "<p><code>code</code> <img class=\"doge-class\" src=\"/assets/dog.png\" alt=\"dog\"><code>ruby\ndoge.code</code>\nhello :unknown-doge:</p>"
+      text = "code ruby\ndoge.code\nhello :unknown-doge:"
 
-      markdown.renderer    = renderer
-      markdown.highlighter = highlighter
-
-      html = "<pre lang=\"ruby\">puts 'Hello, world.'</pre>"
-
-      expect(renderer).to    receive(:render).with('text').and_return(html)
-      expect(highlighter).to receive(:highlight).with("puts 'Hello, world.'", language: :ruby).and_return('<pre>highlighted code</pre>')
-
-      expect(markdown.render('text')).to include('highlighted code')
+      expect(markdown.strip(html, options)).to include(text)
     end
   end
 end
