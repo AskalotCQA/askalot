18f92171c61883b64ab705e36789b860453341e2
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-27 11:34:01 +0100

    Introduce Ratain :dancer:

---
+++ lib/ratain.rb
@@ -0,0 +1,7 @@
+require 'statistics2'
+
+require 'ratain/base'
+
+module Ratain
+  extend Ratain::Base
+end
---
+++ lib/ratain/base.rb
@@ -0,0 +1,14 @@
+module Ratain
+  module Base
+    # Lower bound of Wilson score confidence interval for a Bernoulli parameter
+    def lb_wsci_bp(positive, total, confidence = 0.95)
+      return 0 if total.zero?
+
+      n = total
+      z = Statistics2.pnormaldist(1 - (1 - confidence) / 2)
+      p = 1.0 * positive / n
+
+      (p + z * z / (2 * n) - z * Math.sqrt((p * (1 - p) + z * z / (4 * n)) / n)) / (1 + z * z / n)
+    end
+  end
+end
