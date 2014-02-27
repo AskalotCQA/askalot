01b2fb78292e1536b294301dfaba19d9c7c4e38d
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-27 01:36:38 +0100

    Fix dependency on ActionView for Gemoji renderer

--- lib/redcurtain/renderer/gemoji.rb
+++ lib/redcurtain/renderer/gemoji.rb
@@ -1,5 +1,13 @@
+# TODO(zbell) try to drop ActionView dependency
+
+require 'action_view/helpers/asset_tag_helper'
+require 'action_view/helpers/asset_url_helper'
+
 module Redcurtain::Renderer
   module Gemoji
+    include ActionView::Helpers::AssetTagHelper
+    include ActionView::Helpers::AssetUrlHelper
+
     extend self
 
     def render(content, options = {})
