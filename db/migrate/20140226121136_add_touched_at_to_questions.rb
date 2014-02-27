78e2b201683ca1179b9ff3649ab46f32b263acda
Jozef Harinek <j.harinek@gmail.com>
2014-02-27 10:35:15 +0100

    Fix migration for touched_at in questions

--- db/migrate/20140226121136_add_touched_at_to_questions.rb
+++ db/migrate/20140226121136_add_touched_at_to_questions.rb
@@ -1,5 +1,24 @@
 class AddTouchedAtToQuestions < ActiveRecord::Migration
   def change
-    add_column :questions, :touched_at, :datetime, null: false, default: Time.now
+    add_column :questions, :touched_at, :datetime, null: false, default: '2000-01-01 00:00:00'
+
+    Question.all.each do |question|
+      if question.answers.count == 0
+        question.touched_at = question.updated_at
+      else
+        question.answers.each do |answer|
+          question.touched_at = answer.updated_at if question.touched_at < answer.updated_at
+          answer.comments.each do |comment|
+            question.touched_at = comment.updated_at if question.touched_at < comment.updated_at
+          end
+        end
+      end
+
+      question.comments.each do |comment|
+        question.touched_at = comment.updated_at if question.touched_at < comment.updated_at
+      end
+
+      question.save
+    end
   end
 end
