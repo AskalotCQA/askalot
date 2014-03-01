babb4771243e0604e0efbc4edf73e81c2f8e79f0
MarekGrznar <marek.grznar@gmail.com>
2014-03-01 10:37:52 +0100

    Add default scope for deleted flag in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ActiveRecord::Base
 
   validates :text, presence: true
 
+  default_scope      lambda { where deleted: false }
+
   scope :by,  lambda { |user| where(author: user) }
   scope :for, lambda { |model| where(commentable_type: model.to_s.classify) }
 end
