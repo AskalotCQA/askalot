92c91ecd532ef5307d81d85db359bb899a9a9910
Samuel Molnar <molnar.samuel@gmail.com>
2014-03-02 20:38:31 +0100

    Fix flash variable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,8 +13,7 @@ class CommentsController < ApplicationController
     if @comment.save
       flash[:notice] = t('comment.create.success')
     else
-      # TODO (smolnar) resolve why named argument for flash is nil
-      flash_error_messages_for @comment, flash: flash
+      flash_error_messages_for @comment
     end
 
     redirect_to question_path(@question)
--- app/controllers/concerns/flash.rb
+++ app/controllers/concerns/flash.rb
@@ -1,7 +1,7 @@
 module Concerns::Flash
   extend ActiveSupport::Concern
 
-  def flash_error_messages_for(resource, flash: flash, **options)
+  def flash_error_messages_for(resource, flash: self.flash, **options)
     messages = resource.errors.full_messages
 
     flash[:error] = Array.wrap(flash[:error]) + messages.reject(&:blank?) unless messages.empty?
