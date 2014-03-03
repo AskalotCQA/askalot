9857e32db82c6db89617ee4f1bc665ed9a040999
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-03 04:24:56 +0100

    Update Redcurtain initializer

--- config/initializers/redcurtain.rb
+++ config/initializers/redcurtain.rb
@@ -2,3 +2,42 @@ Redcurtain::Markdown.renderers.unshift(*[
   Redcurtain::Renderer::Linker.new(:user),
   Redcurtain::Renderer::Linker.new(:question)
 ])
+
+[:autolink, :no_images, :no_styles].each do |flag|
+  Redcurtain::Renderer::Redcarpet.defaults[flag] = true
+end
+
+Redcurtain::Renderer::Redcarpet.defaults[:tags] &= [
+  :autolink,
+  :block_code,
+#  :block_html,
+  :block_quote,
+  :codespan,
+#  :doc_footer,
+#  :doc_header,
+#  :double_emphasis,
+  :emphasis,
+#  :entity,
+#  :footnote_ref,
+#  :footnotes,
+#  :footnotes_def,
+#  :header,
+#  :highlight,
+  :hrule,
+#  :image,
+  :linebreak,
+  :link,
+  :list,
+  :list_item,
+  :normal_text,
+  :paragraph,
+  :quote,
+#  :raw_html,
+  :striketrough,
+  :superscript,
+#  :table,
+#  :table_cell,
+#  :table_row,
+#  :triple_emphasis,
+  :underline
+]
