87004e52ccc7da4cfc0eeb8fa6446d0d77f39ddf
Jozef Harinek <j.harinek@gmail.com>
2014-03-03 20:04:32 +0100

    Fix touchable concern

--- app/models/concerns/touchable.rb
+++ app/models/concerns/touchable.rb
@@ -1,39 +1,26 @@
 module Touchable
   extend ActiveSupport::Concern
 
-  # TODO(jharinek) experiment 1
-  #included do
-  #  before_save :touch_touched_at
-  #  after_save :update_touched_at!
-  #end
-  #
-  #def update_touched_at!
-  #  question = self.to_question
-  #
-  #  unless question == self
-  #    question.touched_at = self.updated_at
-  #
-  #    question.save!
-  #  end
-  #end
-  #
-  #private
-  #
-  #def touch_touched_at
-  #  question = self.to_question
-  #
-  #  question.touch(:touched_at) if question == self
-  #end
+  included do
+    before_save :touch_touched_at
+    after_save :update_touched_at!
+  end
 
-  # TODO(jharinek) experiment 2
-  #included do
-  #  after_touch :update_touched_at!
-  #end
-  #
-  #def update_touched_at!
-  #  question = self.to_question
-  #
-  #  question.touched_at = self.updated_at
-  #  question.save!
-  #end
+  def update_touched_at!
+    question = self.to_question
+
+    unless question == self
+      question.touched_at = self.updated_at
+
+      question.save!
+    end
+  end
+
+  private
+
+  def touch_touched_at
+    question = self.to_question
+
+    question.touch(:touched_at) if question == self
+  end
 end
