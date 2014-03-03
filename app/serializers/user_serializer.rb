c7931d67d24b24a41f4ac6cb41b73311dca76cef
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-03 03:04:46 +0100

    Do not make GravaterHelper to extend self

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,10 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :nick, :gravatar_url
 
   def gravatar_url
-    GravatarHelper.gravatar_url(object.email)
+    Helper.gravatar_url(object.email)
+  end
+
+  module Helper
+    extend GravatarHelper
   end
 end
