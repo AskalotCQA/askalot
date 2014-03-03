859c1acb83f82592c2bb24e54c9920a7dfcefc55
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-03 23:21:46 +0100

    Format some code

--- app/controllers/concerns/deleting.rb
+++ app/controllers/concerns/deleting.rb
@@ -2,7 +2,7 @@ module Deleting
   extend ActiveSupport::Concern
 
   def delete
-    @model = controller_name.classify.downcase.to_sym
+    @model     = controller_name.classify.downcase.to_sym
     @deletable = controller_name.classify.constantize.find(params[:id])
 
     if @deletable.mark_as_deleted!(@deletable)
@@ -11,7 +11,7 @@ module Deleting
       flash_error_messages_for @deletable
     end
 
-    if @deletable.is_a?(Question)
+    if @deletable.is_a? Question
       redirect_to questions_path
     else
       redirect_to :back
