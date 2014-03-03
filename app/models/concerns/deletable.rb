6f55e6b4acf92396e473116f14836833ac9cf739
MarekGrznar <marek.grznar@gmail.com>
2014-03-03 02:19:06 +0100

    Add deleted and undeleted scopes

--- app/models/concerns/deletable.rb
+++ app/models/concerns/deletable.rb
@@ -1,6 +1,11 @@
 module Deletable
   extend ActiveSupport::Concern
 
+  included do
+    scope :deleted, lambda { where(deleted: true) }
+    scope :undeleted, lambda { where(deleted: false) }
+  end
+
   def mark_as_deleted!(deletion)
     deletion.deleted = true
     deletion.save!
