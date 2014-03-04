056e510f0bb3aaf9cde807550e64cbd825cf8bb4
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-04 12:57:42 +0100

    Disable new answered questions scope implementation

--- app/models/question.rb
+++ app/models/question.rb
@@ -23,7 +23,7 @@ class Question < ActiveRecord::Base
 
   scope :random,     lambda { select('questions.*, random()').order('random()') }
   scope :unanswered, lambda { includes(:answers).where(answers: { question_id: nil }) }
-  scope :answered,   lambda { joins(:answers).where('questions.id not in (?)', joins(:answers).merge(best_answers).uniq.select('questions.id')).uniq }
+  scope :answered,   lambda { joins(:answers).uniq } #.where('questions.id not in (?)', joins(:answers).merge(best_answers).uniq.select('questions.id')).uniq } # TODO(zbell) User.first.questions.answered fails
   scope :solved,     lambda { joins(:answers).merge(best_answers).uniq }
 
   scope :by, lambda { |user| where(author: user) }
