65c4d04b77effa5bc2827b14535b1b993d289597
Samuel Molnar <molnar.samuel@gmail.com>
2014-03-06 15:02:47 +0100

    Fix select2 helpers

--- spec/support/select2_helper.rb
+++ spec/support/select2_helper.rb
@@ -2,7 +2,7 @@ module Select2Helper
   def fill_in_select2(selector, options = {})
     page.find(:css, "#s2id_#{selector} input.select2-input").set options[:with]
 
-    page.find(:css, 'ul.select2-results .select2-result-label').click
+    page.document.find(:css, 'ul.select2-results .select2-result-label').click
 
     wait_for_remote
   end
