840c9f13f3ae7079f18b157a3d7c29db6283b2bc
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-11 01:35:21 +0100

    Render tooltip on Gemojis

--- lib/redcurtain/renderer/gemoji.rb
+++ lib/redcurtain/renderer/gemoji.rb
@@ -4,13 +4,16 @@ module Redcurtain::Renderer
 
     def render(content, options = {})
       classes = Array.wrap(options[:class] || :gemoji)
-      path    = options[:path] || '/images/gemoji'
+      path    = options[:path]  || '/images/gemoji'
+      title   = options[:title] || lambda { |name| name }
 
       content.to_s.gsub(/(^|[^`]{0,3}\s+):[a-z0-9\+\-_]+:($|[^`]{0,3}+)/) { |match|
         name = match[1..-2]
 
         if Emoji.names.include? name
-          "<img class=\"#{classes.join ' '}\" src=\"#{File.join path, "#{name}.png"}\" alt=\"#{name}\" />"
+          data = "data-title=\"#{title.call name}\" data-toggle=\"tooltip\" data-placement=\"bottom\" " if title.present?
+
+          "<img class=\"#{classes.join ' '}\" src=\"#{File.join path, "#{name}.png"}\" alt=\"#{name}\" #{data}/>"
         else
           match
         end
