cc193c392596357355ad04ae9215cfacd6a17629
MarekGrznar <marek.grznar@gmail.com>
2014-03-11 12:33:46 +0100

    Fix migrations after code review

--- db/migrate/20140310195122_set_not_null_on_created_at_and_updated_at.rb
+++ db/migrate/20140310195122_set_not_null_on_created_at_and_updated_at.rb
@@ -1,11 +1,11 @@
 class SetNotNullOnCreatedAtAndUpdatedAt < ActiveRecord::Migration
   def change
-    columns = [:answer_revisions, :answers, :categories, :changelogs, :comment_revisions, :comments, :favorites, :followings,
+    tables = [:answer_revisions, :answers, :categories, :changelogs, :comment_revisions, :comments, :favorites, :followings,
                :labelings, :labels, :notifications, :question_revisions, :questions, :slido_events, :taggings, :users, :votes, :watchings]
 
-    columns.each do |column|
-      change_column column, :created_at, :timestamp, null: false
-      change_column column, :updated_at, :timestamp, null: false
+    tables.each do |table|
+      change_column table, :created_at, :timestamp, null: false
+      change_column table, :updated_at, :timestamp, null: false
     end
 
     change_column :views, :created_at, :timestamp, null: false
--- db/migrate/20140311001031_add_deletor_and_deleted_at.rb
+++ db/migrate/20140311001031_add_deletor_and_deleted_at.rb
@@ -1,13 +1,10 @@
 class AddDeletorAndDeletedAt < ActiveRecord::Migration
   def change
-
     models = [:answers, :questions, :comments, :evaluations, :labelings, :views, :votes]
 
     models.each do |model|
-      add_column model,  :deleted_at, :timestamp, default: null
-      add_column model,  :deletor_id, :integer
-
-      add_index  model,  :deletor_id
+      add_column model, :deleted_at, :timestamp, default: null
+      add_references model, :deletor, index: true
     end
   end
 end
