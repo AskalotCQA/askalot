9c137ed540c6bc915d00bcea5493748854e8bd49
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-12 16:56:19 +0100

    Add more notification actions

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,5 +3,19 @@ class Notification < ActiveRecord::Base
   belongs_to :initiator, class_name: :User
   belongs_to :notifiable, polymorphic: true
 
-  symbolize :action, in: [:'add-question', :'add-answer', :'add-comment', :'mention-user']
+  symbolize :action, in: [
+    :'create-question',
+    :'create-answer',
+    :'create-comment',
+
+    :'edit-question',
+    :'edit-answer',
+    :'edit-comment',
+
+    :'delete-question',
+    :'delete-answer',
+    :'delete-comment',
+
+    :'mention-user'
+  ]
 end
