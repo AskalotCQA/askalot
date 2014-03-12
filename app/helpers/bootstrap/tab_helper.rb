5d56799a96feda0916b99d25f81fd2291b9351c4
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-13 00:50:18 +0100

    Move tag helpers into separate module

---
+++ app/helpers/bootstrap/tab_helper.rb
@@ -0,0 +1,27 @@
+module Bootstrap::TabHelper
+  def tab_link_tag(title, tab, path, options = {}, &block)
+    classes  = Hash.new
+    defaults = { data: { toggle: :tab, state: true, target: "##{tab}" }}
+
+    options = defaults.deep_merge(options)
+
+    classes.merge! class: :active if params[:tab].to_sym == tab.to_sym
+
+    content_tag :li, classes do
+      block_given? ? yield(options) : link_to(title, path, options)
+    end
+  end
+
+  def tab_link_tag_with_count(body, tab, path, count, options = {})
+    tab_link_tag(body, tab, path, options) do |defaults|
+      link_to_with_count(body, path, count, options.deep_merge(defaults))
+    end
+  end
+
+  def tab_content_tag(tab, options = {}, &block)
+    options.merge! id: tab
+    options.merge! class: "tab-pane #{:active if params[:tab].to_sym == tab.to_sym}"
+
+    content_tag :div, options, &block
+  end
+end
