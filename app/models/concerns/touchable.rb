7083b4f55d5892a1d8a57446a116a4c3d9fb08f3
Jozef Harinek <j.harinek@gmail.com>
2014-03-12 11:41:34 +0100

    Add after create callback to touchable

--- app/models/concerns/touchable.rb
+++ app/models/concerns/touchable.rb
@@ -2,8 +2,9 @@ module Touchable
   extend ActiveSupport::Concern
 
   included do
-    before_save :touch_touched_at
-    after_save  :update_touched_at!
+    before_save  :touch_touched_at
+    after_create :set_touched_at!
+    after_save   :update_touched_at!
   end
 
   def update_touched_at!
@@ -21,6 +22,15 @@ module Touchable
   def touch_touched_at
     question = self.to_question
 
-    question == self && !question.new_record? ? question.touch(:touched_at) : question.touched_at = DateTime.now
+    question == self && !question.new_record? ? question.touch(:touched_at) : question.touched_at = '2000-01-01 00:00:00'
+  end
+
+  def set_touched_at!
+    question = self.to_question
+
+    if question == self
+      question.touched_at = question.updated_at
+      question.save!
+    end
   end
 end
