bd2339f7b21fda6404a17312be66b27bbfb18356
Jozef Harinek <j.harinek@gmail.com>
2014-03-13 17:31:32 +0100

    Refactor touchable to update question only if answer or comment

--- app/models/concerns/touchable.rb
+++ app/models/concerns/touchable.rb
@@ -2,8 +2,6 @@ module Touchable
   extend ActiveSupport::Concern
 
   included do
-    before_save  :touch_touched_at
-    after_create :set_touched_at!
     after_save   :update_touched_at!
   end
 
@@ -13,23 +11,6 @@ module Touchable
     unless question == self
       question.touched_at = self.updated_at
 
-      question.save!
-    end
-  end
-
-  private
-
-  def touch_touched_at
-    question = self.to_question
-
-    question == self && !question.new_record? ? question.touch(:touched_at) : question.touched_at = '2000-01-01 00:00:00'
-  end
-
-  def set_touched_at!
-    question = self.to_question
-
-    if question == self
-      question.touched_at = question.updated_at
       question.save!
     end
   end
