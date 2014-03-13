727e04b858fee27cf0329238f372f30e2d06ee1b
Samuel Molnar <molnar.samuel@gmail.com>
2014-03-13 01:49:36 +0100

    Fix markdown proceesing for empty texts

--- app/controllers/concerns/markdown.rb
+++ app/controllers/concerns/markdown.rb
@@ -2,7 +2,10 @@ module Markdown
   extend ActiveSupport::Concern
 
   def process_markdown_for(resource, attribute: :text, **options, &callback)
-    text   = resource.public_send(attribute)
+    text = resource.public_send(attribute)
+
+    return if text.empty?
+
     result = Markdown::Processor.process(text, current_user, &callback)
 
     resource.public_send(:"#{attribute}=", result)
