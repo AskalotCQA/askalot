4cab6ebb29c2dc372583ec9ebda05d64291be03a
MarekGrznar <marek.grznar@gmail.com>
2014-03-13 13:52:58 +0100

    Fix deleting feature

--- app/models/concerns/deletable.rb
+++ app/models/concerns/deletable.rb
@@ -2,15 +2,38 @@ module Deletable
   extend ActiveSupport::Concern
 
   included do
+    belongs_to :deletor, class_name: :User
+
     scope :deleted,   lambda { where(deleted: true) }
     scope :undeleted, lambda { where(deleted: false) }
 
     default_scope -> { undeleted }
-  end
 
-  def mark_as_deleted!
-    self.deleted = true
+    after_save  :recursion_mark_as_deleted!
+
+    end
+
+  def mark_as_deleted!(user)
+    self.deleted    = true
+    self.deletor    = user
+    self.deleted_at = DateTime.now
 
     self.save!
+  end
+
+  def recursion_mark_as_deleted!
+    if self.deleted?
+      self.reflections.each do |key, target_child|
+        if can_delete target_child
+          self.send(key.to_s).each do |recursion_child|
+            recursion_child.mark_as_deleted! self.deletor
+          end
+        end
+      end
+    end
+  end
+
+  def can_delete(model)
+    model.options[:dependent] == :destroy && model.macro == :has_many && model.klass.column_names.include?('deleted')
   end
 end
