f9defb72abf890716c8b43329bd04d5755329481
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-14 01:06:38 +0100

    Refactor most of the views and styles, fix sextion tags

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,27 @@
 module UsersHelper
   def user_avatar_tag(user, options = {})
-    classes = "user-avatar#{'-small' if options.delete(:small)}"
+    classes = [:'user-avatar'] + Array.wrap(options.delete :class)
+    image   = options[:image] || {}
 
-    if user == :anonymous
-      return content_tag :span, class: classes do
-        gravatar_image_tag 'anonymous@fiit.stuba.sk', options.merge(default: image_url('anonymous.png'), alt: :anonymous)
+    if options[:size]
+      classes << "user-avatar-xs-#{options[:size]}"
+    else
+      classes << case image[:size]
+      when   0.. 20 then :'user-avatar-xs'
+      when  21.. 80 then :'user-avatar-sm'
+      when  81..240 then nil
+      else               :'user-avatar-lg'
       end
     end
 
-    link_to user_path(user.nick), class: classes do
-      gravatar_image_tag user.gravatar_email, options.merge(alt: user.nick)
+    if user == :anonymous
+      return content_tag :span, class: classes do
+        gravatar_image_tag 'anonymous@fiit.stuba.sk', image.merge(default: image_url('anonymous.png'), alt: :anonymous)
+      end
+    end
+
+    link_to options[:url] || user_path(user.nick), class: classes do
+      gravatar_image_tag user.gravatar_email, image.merge(alt: user.nick)
     end
   end
 
