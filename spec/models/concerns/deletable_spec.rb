b91b066f5464c08509520b940099bc4fdf196bb8
MarekGrznar <marek.grznar@gmail.com>
2014-03-15 11:21:39 +0100

    Add test for finding associations and setting deleting attributes

--- spec/models/concerns/deletable_spec.rb
+++ spec/models/concerns/deletable_spec.rb
@@ -6,7 +6,7 @@ shared_examples_for Deletable do
   let(:resource) { create factory }
 
   describe '.mark_as_deleted_by!' do
-    it 'set deleted, deletor and deleted_at atribute' do
+    it 'set deleted, deletor and deleted_at attribute' do
       user = create :user
       now = DateTime.now.in_time_zone
 
@@ -15,6 +15,30 @@ shared_examples_for Deletable do
       expect(resource.deleted).to eql(true)
       expect(resource.deletor).to eql(user)
       expect(resource.deleted_at.to_s).to eql(now.to_s)
+    end
+  end
+
+  describe '.mark_as_deleted_recursive!' do
+    let!(:comment){ create :comment, commentable: resource }
+    let!(:evaluation){ create :evaluation, evaluable: resource }
+
+    it 'find associations and set deleted, deletor and deleted_at attribute' do
+      user = create :user
+      now = DateTime.now.in_time_zone
+
+      resource.mark_as_deleted_by!(user)
+
+      expect(resource.deleted).to eql(true)
+      expect(resource.deletor).to eql(user)
+      expect(resource.deleted_at.to_s).to eql(now.to_s)
+
+      expect(resource.comments.first.deleted).to eql(true)
+      expect(resource.comments.first.deletor).to eql(user)
+      expect(resource.comments.first.deleted_at.to_s).to eql(now.to_s)
+
+      expect(resource.evaluations.first.deleted).to eql(true)
+      expect(resource.evaluations.first.deletor).to eql(user)
+      expect(resource.evaluations.first.deleted_at.to_s).to eql(now.to_s)
     end
   end
 end
