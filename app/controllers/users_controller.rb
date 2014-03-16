b255682f91f25d4b62d6cd8bb2b10b9ae7b81d13
Rastislav Dobšovič <rastislav.dobsovic@gmail.com>
2014-03-16 22:41:52 +0100

    Add view for answers tab in users profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_nick params[:nick]
 
-    @questions = @user.questions
-    @answers   = @user.answers
-    @favorites = Question.favorite_by(@user)
+    @questions = @user.questions.order(created_at: :desc)
+    @answers   = @user.answers.order(created_at: :desc)
+    @favorites = Question.favorite_by(@user).order(created_at: :desc)
 
     @questions = @questions.page(params[:page]).per(10)
     @answers   = @answers.page(params[:page]).per(10)
