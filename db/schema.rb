7f8c8f499433a7fa2efe6de7108c5f21b93bc8be
Samuel Molnar <molnar.samuel@gmail.com>
2014-03-16 23:44:45 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -307,8 +307,8 @@ ActiveRecord::Schema.define(version: 20140315163927) do
 
   create_table "tags", force: true do |t|
     t.string   "name",       null: false
-    t.datetime "updated_at", null: false
     t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
