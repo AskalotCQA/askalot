a60b17cc1f6391059620b95058cc99f7c6262cf2
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-18 13:31:30 +0100

    Refactor and improve users helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
     image   = options[:image] || {}
 
     if options[:size]
-      classes << "user-avatar-xs-#{options[:size]}"
+      classes << "user-avatar-#{options[:size]}"
     else
       classes << case image[:size]
       when   0.. 20 then :'user-avatar-xs'
@@ -14,20 +14,28 @@ module UsersHelper
       end
     end
 
-    if user == :anonymous
-      return content_tag :span, class: classes do
-        gravatar_image_tag 'anonymous@fiit.stuba.sk', image.merge(default: image_url('anonymous.png'), alt: :anonymous)
-      end
-    end
+    return content_tag :span, anonymous_gravatar_image_tag(image), class: classes if user == :anonymous
 
     link_to options[:url] || user_path(user.nick), class: classes do
       gravatar_image_tag user.gravatar_email, image.merge(alt: user.nick)
     end
   end
 
+  def icon_link_to_user(user, options = {})
+    return content_tag :span, anonymous_gravatar_image_tag(image), options if user == :anonymous
+
+    link_to gravatar_image_tag(user.gravatar_email, class: 'user-avatar-icon', alt: user.nick), user_path(user.nick), options
+  end
+
   def link_to_user(user, options = {})
-    return t('user.anonymous') if user == :anonymous
+    return content_tag :span, t('user.anonymous'), options if user == :anonymous
 
     link_to user.nick, user_path(user.nick), options
+  end
+
+  private
+
+  def anonymous_gravatar_image_tag(options = {})
+    gravatar_image_tag 'anonymous@fiit.stuba.sk', options.merge(default: image_url('anonymous.png'), alt: :anonymous)
   end
 end
