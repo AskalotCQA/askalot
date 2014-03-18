6d0eb3e1babc6d30a48280b6f338a52bb8214e81
Samuel Molnar <molnar.samuel@gmail.com>
2014-03-18 19:58:10 +0100

    Prepare specs for watching

---
+++ spec/features/watching_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'Watching' do
+  context 'with question' do
+    it 'registers watching for question' do
+      pending
+    end
+  end
+
+  context 'with tag' do
+    it 'registers watching for tag' do
+      pending
+    end
+  end
+
+  context 'with category' do
+    it 'registers watching for category' do
+      pending
+    end
+  end
+end
