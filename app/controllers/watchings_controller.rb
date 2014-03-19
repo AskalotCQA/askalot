c1432c5b3ce4b7a79da353bc902c88b7fb11ab85
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-19 14:57:13 +0100

    Improve paging by current tab in controllers

--- app/controllers/watchings_controller.rb
+++ app/controllers/watchings_controller.rb
@@ -1,7 +1,17 @@
 class WatchingsController < ApplicationController
+  include Tabbing
+
+  default_tab :questions, only: :index
+
   before_action :authenticate_user!
 
   def index
-    @watchings = Watching.where(watcher: current_user).page(params[:page]).per(20)
+    count = 25
+
+    @watchings = Watching.where(watcher: current_user).order(created_at: :desc)
+
+    @questions  = @watchings.of(:question).page(tab_page :questions).per(count)
+    @categories = @watchings.of(:category).page(tab_page :categories).per(count)
+    @tags       = @watchings.of(:tag).page(tab_page :tags).per(count)
   end
 end
