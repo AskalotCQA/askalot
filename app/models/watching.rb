61c50525df7f7d94b9b26da19eb66023caa76857
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-19 14:56:22 +0100

    Refactor watching scopes

--- app/models/watching.rb
+++ app/models/watching.rb
@@ -2,6 +2,6 @@ class Watching < ActiveRecord::Base
   belongs_to :watcher, class_name: :User
   belongs_to :watchable, polymorphic: true
 
-  scope :by,  lambda { |user| where(watcher: user) }
-  scope :for, lambda { |model| where(watchable_type: model.to_s.classify) }
+  scope :by, lambda { |user| where(watcher: user) }
+  scope :of, lambda { |model| where(watchable_type: model.to_s.classify) }
 end
