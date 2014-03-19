d2a29aaa334ebfeb9038db2551c269a223b2aaa7
Samuel Molnar <molnar.samuel@gmail.com>
2014-03-19 21:26:09 +0100

    Fix specs

--- spec/support/capybara_helpers.rb
+++ spec/support/capybara_helpers.rb
@@ -3,9 +3,9 @@ module CapybaraHelpers
     begin
       find(:link, locator, options).click
     rescue Exception => e
-      return page.evaluate_script("$(\"a:contains('#{locator}')\").click()") if example.metadata[:js]
+      raise e unless example.metadata[:js]
 
-      raise e
+      Timeout.timeout(3) { page.evaluate_script("$(\"a:contains('#{locator}')\").click()") } rescue raise e
     end
   end
 
