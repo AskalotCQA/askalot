b9155e4a3249dca5849c449695af63912ee4642c
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-20 21:28:37 +0100

    Consider deleted watchables in watching model

--- app/models/watching.rb
+++ app/models/watching.rb
@@ -1,6 +1,6 @@
 class Watching < ActiveRecord::Base
   belongs_to :watcher, class_name: :User
-  belongs_to :watchable, polymorphic: true
+  belongs_to :watchable, -> { deleted_or_not if respond_to? :deleted_or_not }, polymorphic: true
 
   scope :by, lambda { |user| where(watcher: user) }
   scope :of, lambda { |model| where(watchable_type: model.to_s.classify) }
