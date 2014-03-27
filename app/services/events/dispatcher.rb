1b781db25e20b880e1f992abef8c3951c7ff99f2
Samuel Molnar <molnar.samuel@gmail.com>
2014-03-27 12:01:53 +0100

    Refactor events dispatching

---
+++ app/services/events/dispatcher.rb
@@ -0,0 +1,27 @@
+module Events
+  module Dispatcher
+    extend self
+
+    def listeners
+      @listeners ||= []
+    end
+
+    def subscribe(listener)
+      listeners << listener
+    end
+
+    def unsubscribe(listener)
+      listeners.delete(listener)
+    end
+
+    def unsubscribe_all
+      @notifiers = []
+    end
+
+    def dispatch(action, initiator, resource, options = {})
+      listeners.each do |listener|
+        listener.publish(action, initiator, resource, options)
+      end
+    end
+  end
+end
---
+++ config/initializers/events/dispatcher.rb
@@ -0,0 +1,1 @@
+Events::Dispatcher.subscribe Notifications::Notifier
