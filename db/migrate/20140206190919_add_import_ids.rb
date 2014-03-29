bd343691fdd96cfcea4ab91d2e9644708adcb526
Samuel Molnar <molnar.samuel@gmail.com>
2014-03-29 19:00:53 +0100

    Refactor migrations

--- db/migrate/20140206190919_add_import_ids.rb
+++ db/migrate/20140206190919_add_import_ids.rb
@@ -1,15 +1,13 @@
-class AddImportIds < ActiveRecord::Migration
+class AddStackExchangeIds < ActiveRecord::Migration
   def change
-    add_column :users, :imported_id, :integer
-    add_index :users, :imported_id
+    add_column :users,     :stack_exchange_uuid, :integer
+    add_column :questions, :stack_exchange_uuid, :integer
+    add_column :answers,   :stack_exchange_uuid, :integer
+    add_column :comments,  :stack_exchange_uuid, :integer
 
-    add_column :questions, :imported_id, :integer
-    add_index :questions, :imported_id
-
-    add_column :answers, :imported_id, :integer
-    add_index :answers, :imported_id
-
-    add_column :comments, :imported_id, :integer
-    add_index :comments, :imported_id
+    add_index :users,     :stack_exchange_uuid, unique: true
+    add_index :questions, :stack_exchange_uuid, unique: true
+    add_index :answers,   :stack_exchange_uuid, unique: true
+    add_index :comments,  :stack_exchange_uuid, unique: true
   end
 end
