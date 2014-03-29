f4e480125e7b93555fb488ea3989eb543e875b08
MarekGrznar <marek.grznar@gmail.com>
2014-03-29 13:40:49 +0100

    Add follow function to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,14 @@ class UsersController < ApplicationController
     redirect_to edit_user_registration_path(tab: params[:tab])
   end
 
+  def follow
+    @followee = User.find(params[:id])
+
+    current_user.toggle_following_by! @followee
+
+    render 'followables/follow', formats: :js
+  end
+
   def suggest
     @users = User.where('nick like ?', "#{params[:q]}%")
 
