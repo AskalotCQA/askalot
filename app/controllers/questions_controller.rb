ec394293a22a8f41d78d6decbedeb0724dcdd7a2
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-31 23:44:44 +0200

    Fix by_votes ordering scope

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,10 +14,10 @@ class QuestionsController < ApplicationController
 
   def index
     @questions = case params[:tab].to_sym
-                 when :unanswered then Question.unanswered.order('questions.votes_lb_wsci_bp desc, questions.created_at desc')
-                 when :answered   then Question.answered_but_not_best.by_votes.order(created_at: :desc)
-                 when :solved     then Question.solved.by_votes.order(created_at: :desc)
-                 when :favored    then Question.favored.by_votes.order(created_at: :desc)
+                 when :unanswered then Question.unanswered.by_votes
+                 when :answered   then Question.answered_but_not_best.by_votes
+                 when :solved     then Question.solved.by_votes
+                 when :favored    then Question.favored.by_votes
                  else Question.recent
                  end
 
