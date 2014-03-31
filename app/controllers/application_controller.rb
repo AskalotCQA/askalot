37ba1067c087a595d046c2d32562459addb08f3f
MarekGrznar <marek.grznar@gmail.com>
2014-03-31 14:20:15 +0200

    Add partial for logging events for google analytics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protected
 
   # concerns order is significant
+  include Concerns::Analytics
   include Concerns::Security
   include Concerns::Log
   include Concerns::Flash
