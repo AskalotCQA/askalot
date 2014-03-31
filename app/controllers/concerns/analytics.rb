212aee58b2d3fe6dd38168a97663391a414c0aaf
MarekGrznar <marek.grznar@gmail.com>
2014-03-31 14:19:26 +0200

    Add concern for logging events in controller for google analytics

---
+++ app/controllers/concerns/analytics.rb
@@ -0,0 +1,10 @@
+module Concerns::Analytics
+  extend ActiveSupport::Concern
+
+  protected
+  # GA event logger
+  def log_event(category, action, label = nil)
+    session[:events] ||= Array.new
+    session[:events] << {:category => category, :action => action, :label => label}
+  end
+end
