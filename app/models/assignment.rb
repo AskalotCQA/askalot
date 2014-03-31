a0e39c2c493ba80afbfdf0ff859cb0570f4f02a3
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-31 23:52:59 +0200

    Format some code

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,5 +1,5 @@
 class Assignment < ActiveRecord::Base
-  belongs_to :role
   belongs_to :user
   belongs_to :category
+  belongs_to :role
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,6 @@
 class Role < ActiveRecord::Base
   has_many :assignments, dependent: :restrict_with_exception
-
-  has_many :users,      through: :assignments
+  has_many :users, through: :assignments
   has_many :categories, through: :assignments
 
   validates :name, presence: true
