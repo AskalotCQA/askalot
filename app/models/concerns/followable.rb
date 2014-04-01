97127b72fc733b93c03ccf3482058bf6b04c9fb0
Pavol Zbell <pavol.zbell@gmail.com>
2014-04-01 10:57:49 +0200

    Clean up models related to following

--- app/models/concerns/followable.rb
+++ app/models/concerns/followable.rb
@@ -2,16 +2,11 @@ module Followable
   extend ActiveSupport::Concern
 
   included do
-    has_many :reverse_followings, foreign_key: :followee_id, class_name: :Following, dependent: :destroy
     has_many :followings, foreign_key: :follower_id, dependent: :destroy
+    has_many :reverse_followings, foreign_key: :followee_id, class_name: :Following, dependent: :destroy
 
     has_many :followees, through: :followings, source: :followee
     has_many :followers, through: :reverse_followings, source: :follower
-  end
-
-  def toggle_following_by!(user)
-    return follow! user unless following?(user)
-    unfollow! user
   end
 
   def following?(user)
@@ -24,5 +19,11 @@ module Followable
 
   def unfollow!(user)
     followings.find_by(followee: user).destroy!
+  end
+
+  def toggle_following_by!(user)
+    return follow! user unless following?(user)
+
+    unfollow! user
   end
 end
--- app/models/following.rb
+++ app/models/following.rb
@@ -2,8 +2,6 @@ class Following < ActiveRecord::Base
   belongs_to :follower, class_name: :User, counter_cache: :followees_count
   belongs_to :followee, class_name: :User, counter_cache: :followers_count
 
-  validates :follower_id, presence: true
   validates :followee_id, presence: true
-
-  scope :by, lambda { |user| where(follower: user) }
+  validates :follower_id, presence: true
 end
