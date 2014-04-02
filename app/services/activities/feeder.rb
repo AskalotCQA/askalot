d14e0cc7d33b499811ab08213b60093ffc751ec3
Dušan Poizl <nou.spiro@gmail.com>
2014-04-02 16:01:28 +0200

    Create activity for every resource

--- app/services/activities/feeder.rb
+++ app/services/activities/feeder.rb
@@ -3,7 +3,7 @@ module Activities
     extend self
 
     def publish(action, initiator, resource, options = {})
-      Activity.create!(action: action, initiator: initiator, resource: resource) if resource.class != View
+      Activity.create!(action: action, initiator: initiator, resource: resource)
     end
   end
 end
