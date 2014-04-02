9654e08b3b7d394a62b82b1d9f3822fc9a832e2c
Dušan Poizl <nou.spiro@gmail.com>
2014-04-02 17:28:48 +0200

    Add tests for activities

---
+++ spec/features/activities_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe 'Acitivities' do
+  let(:user)      { create :user }
+  let!(:question) { create :question, :with_tags, author: user }
+  let!(:category) { create :category }
+
+  before :each do
+    login_as user
+  end
+
+  context 'create activity' do
+    it 'when asking question', js: true do
+      visit root_path
+
+      click_link 'Opýtať sa otázku'
+
+      select category.name, from: 'question_category_id'
+
+      fill_in 'question_title', with: 'Lorem ipsum?'
+      fill_in 'question_text',  with: 'Lorem ipsum'
+
+      click_button 'Opýtať'
+
+      activity = user.activities.last
+
+      expect(activity.resource).to  eql(Question.last)
+      expect(activity.action).to    eql(:create)
+      expect(activity.initiator).to eql(user)
+    end
+
+    it 'when answering', js: true do
+      visit root_path
+
+      click_link 'Otázky'
+      click_link question.title
+
+      fill_in 'answer_text', with: 'Hey, look at this.'
+
+      click_button 'Odpovedať'
+
+      activity = user.activities.last
+
+      expect(activity.resource).to  eql(Answer.last)
+      expect(activity.action).to    eql(:create)
+      expect(activity.initiator).to eql(user)
+    end
+
+    it 'when editing question', js: true do
+      visit root_path
+
+      click_link 'Otázky'
+      click_link question.title
+
+      click_link "question-#{question.id}-edit-modal"
+
+      within "#question-#{question.id}-editing" do
+        fill_in 'question_title', with: 'Elasticsearch problem'
+        fill_in 'question_text',  with: 'I have a problem with Elasticsearch Client in Ruby.'
+
+        fill_in_select2 'question_tag_list', with: 'elasticsearch'
+        fill_in_select2 'question_tag_list', with: 'ruby'
+
+        click_button 'Uložiť'
+      end
+
+      activity = user.activities.last
+
+      expect(activity.resource).to  eql(question)
+      expect(activity.action).to    eql(:update)
+      expect(activity.initiator).to eql(user)
+    end
+  end
+end
