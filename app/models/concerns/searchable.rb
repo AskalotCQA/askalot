bd96bdba8543595632925947b83dc26a8a465a69
Samuel Molnar <molnar.samuel@gmail.com>
2014-04-03 18:00:11 +0200

    Add search and callbacks for Searchable module

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -3,5 +3,25 @@ module Searchable
 
   included do
     include Probe
+
+    after_save do
+      # TODO (smolnar) consider update
+      self.class.probe.index.import(self)
+    end
+
+    after_destroy do
+      self.class.probe.index.import(self, action: :delete)
+    end
+  end
+
+  module ClassMethods
+    def search(query = {})
+      size = query.delete(:per_page) || 25
+      from = (query.delete(:page) || 0) * size
+
+      results = probe.search({ from: from, size: size }.merge(query))
+
+      results.map { |result| find(result.id) }
+    end
   end
 end
