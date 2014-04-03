f01e89533c527a8b4a3ad260ece592d7b745b2bc
Samuel Molnar <molnar.samuel@gmail.com>
2014-04-03 02:32:41 +0200

    Correcly set index source for bulk actions

--- lib/probe/import/bulk.rb
+++ lib/probe/import/bulk.rb
@@ -6,7 +6,7 @@ module Probe
           request = documents.map do |document|
             [
               { action => { _index: index.name, _type: index.type, _id: document.id }},
-              { index.type => document.to_mapping }
+              document.to_mapping
             ]
           end.flatten
 
