0c4e566e3670de69670446bb77817fae0cda90dc
Samuel Molnar <molnar.samuel@gmail.com>
2014-04-03 20:24:39 +0200

    Fix migration

--- db/migrate/20140403181335_add_author_to_taggings.rb
+++ db/migrate/20140403181335_add_author_to_taggings.rb
@@ -1,6 +1,6 @@
 class AddAuthorToTaggings < ActiveRecord::Migration
   def change
-    add_column :taggings, :author_id, :integer
+    add_column :taggings, :author_id, :integer, null: false
 
     add_index :taggings, :author_id
     add_index :taggings, [:tag_id, :question_id, :author_id], unique: true
