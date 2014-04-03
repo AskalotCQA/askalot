d4e4117faa653397291a1d89e95625b0a8c2a905
Samuel Molnar <molnar.samuel@gmail.com>
2014-04-03 20:25:28 +0200

    Fix indices for tagging

--- db/migrate/20140403181644_index_taggings_on_question_id.rb
+++ db/migrate/20140403181644_index_taggings_on_question_id.rb
@@ -2,5 +2,6 @@ class IndexTaggingsOnQuestionId < ActiveRecord::Migration
   def change
     add_index :taggings, :question_id
     add_index :taggings, [:tag_id, :question_id] # TODO (smolnar) cannot create unique index since some records might be marked as deleted
+    add_index :taggings, [:tag_id, :question_id, :author_id]
   end
 end
