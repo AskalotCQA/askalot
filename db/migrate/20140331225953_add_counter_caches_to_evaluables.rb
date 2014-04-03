91598de0b0e887c53115fc465f91b88fdeb5f0fc
Pavol Zbell <pavol.zbell@gmail.com>
2014-04-03 20:52:40 +0200

    Refactor some migrations

--- db/migrate/20140331225953_add_counter_caches_to_evaluables.rb
+++ db/migrate/20140331225953_add_counter_caches_to_evaluables.rb
@@ -1,10 +1,10 @@
 class AddCounterCachesToEvaluables < ActiveRecord::Migration
-  def self.up
+  def up
     add_counter :answers,   :evaluations
     add_counter :questions, :evaluations
   end
 
-  def self.down
+  def down
     remove_counter :answers,   :evaluations
     remove_counter :questions, :evaluations
   end
