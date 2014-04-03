d541e594439db2c7360921d7ac272e2b982b7244
Samuel Molnar <molnar.samuel@gmail.com>
2014-04-03 20:40:26 +0200

    Fix migrations for author in Tagging

--- db/migrate/20140403181335_add_author_to_taggings.rb
+++ db/migrate/20140403181335_add_author_to_taggings.rb
@@ -1,8 +1,16 @@
 class AddAuthorToTaggings < ActiveRecord::Migration
   def change
-    add_column :taggings, :author_id, :integer, null: false
+    add_column :taggings, :author_id, :integer
+
+    Question.unscoped.find_each do |question|
+      question.taggings.unscoped.each do |tagging|
+        tagging.update_attributes!(author_id: question.author_id)
+      end
+    end
+
+    change_column :taggings, :author_id, :integer, null: false
 
     add_index :taggings, :author_id
-    add_index :taggings, [:tag_id, :question_id, :author_id], unique: true
+    add_index :taggings, [:tag_id, :question_id, :author_id] # TODO (smolnar) cannot create index on deletable
   end
 end
