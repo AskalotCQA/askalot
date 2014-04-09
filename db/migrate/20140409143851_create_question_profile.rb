e0543225aeb1ba7965fe16ab1adec6ae2a8f8b3b
MarekGrznar <marek.grznar@gmail.com>
2014-04-09 16:58:31 +0200

    Add migration for question profile

---
+++ db/migrate/20140409143851_create_question_profile.rb
@@ -0,0 +1,17 @@
+class CreateQuestionProfile < ActiveRecord::Migration
+  def change
+    create_table :question_profiles do |t|
+      t.references :question, null: false
+
+      t.string :attribute
+      t.float :value
+      t.float :probability
+      t.string :source
+
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
+    end
+
+    add_index :question_profiles, :question_id
+  end
+end
