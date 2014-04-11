c6e077d57006e62d1311708b29fdc24d39d67cb6
Samuel Molnar <molnar.samuel@gmail.com>
2014-04-11 15:12:18 +0200

    Implement interest feeder for User profile

--- lib/yeast/user_feeder.rb
+++ lib/yeast/user_feeder.rb
@@ -3,8 +3,31 @@ module Yeast
     extend self
 
     def publish(action, initiator, resource, options = {})
-      # TODO
-      puts "Recieved '#{action}' from #{initiator.name} on #{resource} at #{Time.now}"
+      return unless action == :create
+
+      question = nil
+
+      question = resource if resource.class == Question
+      question = resource.votable if resource.class == Vote && resource.votable.class == Question
+      question = resource.quesiton if resource.class == Favorite
+
+      if resource.class == Question
+        question.tags.each do |tag|
+          profile = User::Profile.find_or_create_by! targetable: tag, user: question.author, property: :interest, source: :QE
+
+          profile.update_attributes!(value: 0.5, probability: 0)
+        end
+      end
+
+      if question
+        question.tags.each do |tag|
+          profile = User::Profile.find_or_create_by! targetable: tag, user: question.author, property: :interest, source: :QE
+
+          value = question.profiles.as(:QE).for(:quality).first.value + profile.value
+
+          profile.update_attributes!(value: value)
+        end
+      end
     end
   end
 end
