078c0911aa61cf83b8b5a1172441fa73e26bd3d7
Pavol Zbell <pavol.zbell@gmail.com>
2014-04-19 21:31:02 +0200

    Move tags extractor from Taggable into services

---
+++ spec/services/tags/extractor_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Tags::Extractor do
+  let(:extractor) { described_class }
+
+  describe '.extract' do
+    it 'extracts tags from string' do
+      values = 'a, b, c '
+
+      expect(extractor.extract(values)).to eql(['a', 'b', 'c'])
+    end
+
+    it 'extract tags from array' do
+      values = ['a', :b, ' c']
+
+      expect(extractor.extract(values)).to eql(['a', 'b', 'c'])
+    end
+  end
+end
