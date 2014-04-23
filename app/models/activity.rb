14a46f5186b86c4d332933ebc9995b39e17b79b0
Pavol Zbell <pavol.zbell@gmail.com>
2014-04-23 21:50:08 +0200

    Format activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -9,6 +9,10 @@ class Activity < ActiveRecord::Base
 
   default_scope -> { where.not(action: :mention).where(resource_type: [Answer, Comment, Evaluation, Question], anonymous: false) }
 
+  scope :global, -> { unscope(where: :anonymous) }
+
+  scope :by_followees_of, lambda { |user| where(initiator: user.followees.pluck(:followee_id)) }
+
   scope :of, lambda { |user| where(initiator: user) }
 
   symbolize :action, in: ACTIONS
@@ -20,7 +24,4 @@ class Activity < ActiveRecord::Base
 
     data.inject({}) { |result, (date, count)| result.tap { result[date.to_time.to_i] = count }}
   end
-
-  scope :global, -> { unscope(where: :anonymous) }
-  scope :by_followees_of, lambda { |user| where(initiator: user.followees.pluck(:followee_id)) }
 end
