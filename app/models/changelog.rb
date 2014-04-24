8ecf0ced42de4a7128234e3ca8e6c77d0c90ec10
Pavol Zbell <pavol.zbell@gmail.com>
2014-04-24 19:27:42 +0200

    Make changelogs comparable

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -1,7 +1,17 @@
 class Changelog < ActiveRecord::Base
+  include Comparable
+
   validates :version, presence: true, uniqueness: true
+
+  def <=>(changelog)
+    -(self.to_version <=> changelog.to_version)
+  end
 
   def identifier
     @identifier ||= version.gsub(/\./, '-')
+  end
+
+  def to_version
+    @version ||= Gem::Version.create self.version
   end
 end
