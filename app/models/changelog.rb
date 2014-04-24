64844f7f524654e35165f727ad7f19e63708ef95
Pavol Zbell <pavol.zbell@gmail.com>
2014-04-24 18:48:56 +0200

    Update changelog model

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -1,6 +1,4 @@
 class Changelog < ActiveRecord::Base
-  validates :title,   presence: true
-  validates :text,    presence: true
   validates :version, presence: true, uniqueness: true
 
   def identifier
