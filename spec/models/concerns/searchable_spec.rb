2415e345449765b800a30a4e638e669be6ce7f37
Samuel Molnar <molnar.samuel@gmail.com>
2014-05-14 10:34:41 +0200

    Fix specs

--- spec/models/concerns/searchable_spec.rb
+++ spec/models/concerns/searchable_spec.rb
@@ -1,18 +1,17 @@
 require 'spec_helper'
 
 shared_examples_for Searchable do
-  let(:model)    { described_class }
-  let(:factory)  { model.name.underscore.to_sym }
+  let(:model)   { described_class }
+  let(:factory) { model.name.underscore.to_sym }
   let(:records) { 10.times.map { |record| create factory } }
 
   before :each do
+    model.probe.index.delete
     model.probe.index.create
 
     records
 
     model.probe.index.flush
-
-    sleep 2
   end
 
   after :each do
@@ -20,6 +19,12 @@ shared_examples_for Searchable do
   end
 
   describe '.search' do
+    it 'searches records' do
+      results = model.search
+
+      expect(results.sort.to_a).to eql(records.sort.to_a)
+    end
+
     it 'paginates records' do
       results = model.search(page: 0, per_page: 5)
 
