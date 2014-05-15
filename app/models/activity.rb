ed688633d1ba0cf6e5ac0fb8fa96ecf6e73569d1
Pavol Zbell <pavol.zbell@gmail.com>
2014-05-15 10:42:50 +0200

    Update translations for activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -9,11 +9,9 @@ class Activity < ActiveRecord::Base
 
   default_scope -> { where.not(action: :mention).where(resource_type: [Answer, Comment, Evaluation, Labeling, Question], anonymous: false) }
 
-  scope :global, -> { unscope(where: :anonymous) }
-
+  scope :global,          lambda { unscope(where: :anonymous) }
+  scope :of,              lambda { |user| where(initiator: user) }
   scope :by_followees_of, lambda { |user| where(initiator: user.followees.pluck(:followee_id)) }
-
-  scope :of, lambda { |user| where(initiator: user) }
 
   symbolize :action, in: ACTIONS
 
