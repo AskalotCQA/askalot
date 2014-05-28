7a054c0b793bd1aa5bef62b5df6e5b4b51040384
Jozef Harinek <j.harinek@gmail.com>
2014-05-28 23:39:59 +0200

    Implement actions in groups controller

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,33 @@
+class GroupsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(create_params)
+
+    if @group.save
+      flash[:notice] = t('group.create.success')
+
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @group     = Group.find(params[:id])
+    @documents = @group.documents
+  end
+
+  def index
+    @groups = Group.all
+  end
+
+  private
+  def create_params
+    params.require(:group).permit(:title, :description, :visibility).merge(owner: current_user)
+  end
+end
