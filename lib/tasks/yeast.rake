8ada89161aae19d60641e9f16d3705ce8bf88af3
Samuel Molnar <molnar.samuel@gmail.com>
2014-06-04 17:30:36 +0200

    Implement component architecture for evalutation

--- lib/tasks/yeast.rake
+++ lib/tasks/yeast.rake
@@ -21,4 +21,91 @@ namespace :yeast do
 
     Yeast.run
   end
+
+  namespace :evaluate do
+    desc 'Evaluate with TF-IDF'
+    task tf_ifd: :environment do
+      Events::Dispatcher.unsubscribe_all
+      Events::Dispatcher.subscribe Yeast::EvaluationFeeder
+
+      Yeast::EvaluationFeeder.strategy = ->(question, terms) {
+        {
+          sort: {
+            _script: {
+              script: "
+              result = doc.score;
+              size   = _index.numDocs();
+
+              foreach(term : terms) {
+                frequency       = _index['title'][term].tf() + _index['text'][term].tf() + _index['tags'][term].tf();
+                total_frequency = _index['title'][term].ttf() + _index['text'][term].ttf() + _index['tags'][term].ttf();
+
+                if (frequency > 0 && total_frequency > 0) {
+                  result += frequency * log((size / total_frequency) + 1);
+                }
+              }
+
+              return result;
+              ",
+                type: :number,
+                order: :desc,
+                params: {
+                  terms: terms
+                }
+            }
+          }
+        }
+      }
+
+      Yeast.run
+    end
+
+    desc 'Evaluate with TF-IDF and interest'
+    task tf_ifd_with_interest: :environment do
+      Events::Dispatcher.unsubscribe_all
+      Events::Dispatcher.subscribe Yeast::EvaluationFeeder
+
+      Yeast::EvaluationFeeder.strategy = -> (question, terms) {
+        tags = question.author.profiles.where(property: :interest).order(:value).limit(10).map { |p| { name: p.targetable.name, value: p.value } }
+
+        {
+          sort: {
+            _script: {
+              script: "
+              result = doc.score;
+              size   = _index.numDocs();
+
+              foreach(term : terms) {
+                frequency       = _index['title'][term].tf() + _index['text'][term].tf() + _index['tags'][term].tf();
+                total_frequency = _index['title'][term].ttf() + _index['text'][term].ttf() + _index['tags'][term].ttf();
+
+                if (frequency > 0 && total_frequency > 0) {
+                  result += frequency * log((size / total_frequency) + 1);
+                }
+              }
+
+              foreach(tag : tags) {
+                name = tag['name'];
+
+                if (_index['tags'][name].tf() > 0) {
+                  result += tag['value'];
+                }
+              }
+
+              return result;
+              ",
+                type: :number,
+                order: :desc,
+                params: {
+                  terms: terms,
+                  tags: tags
+                }
+            }
+          }
+        }
+      }
+
+      Yeast.run
+    end
+  end
 end
--- lib/yeast/evaluation_feeder.rb
+++ lib/yeast/evaluation_feeder.rb
@@ -2,7 +2,7 @@ module Yeast
   module EvaluationFeeder
     extend self
 
-    attr_accessor :total_recall, :total_precision, :number_of_observations
+    attr_accessor :strategy, :total_recall, :total_precision, :number_of_observations
 
     def total_precision
       @total_precision ||= 0
@@ -16,6 +16,10 @@ module Yeast
       @number_of_observations ||= 0
     end
 
+    def strategy
+      @strategy ||= Hash.new
+    end
+
     def publish(action, initiator, resource, options = {})
       if resource.is_a? Question
         return unless resource.stack_exchange_duplicate
@@ -26,7 +30,6 @@ module Yeast
 
         return unless terms
 
-        tags      = question.author.profiles.where(property: :interest).order(:value).limit(10).map { |p| { name: p.targetable.name, value: p.value } }
         query     = {
           query: {
             bool: {
@@ -40,46 +43,8 @@ module Yeast
           query[:query][:bool][:should] << { term: { tags: term }}
         end
 
-        query.deep_merge!({
-          sort: {
-            _script: {
-              script: "
-              result = doc.score;
-              size   = _index.numDocs();
-
-              foreach(term : terms) {
-                frequency       = _index['title'][term].tf() + _index['text'][term].tf() + _index['tags'][term].tf();
-                total_frequency = _index['title'][term].ttf() + _index['text'][term].ttf() + _index['tags'][term].ttf();
-
-                if (frequency > 0 && total_frequency > 0) {
-                  result += frequency * log((size / total_frequency) + 1);
-                }
-              }
-
-              return result;
-
-              foreach(tag : tags) {
-                name = tag['name'];
-
-                if (_index['tags'][name].tf() > 0) {
-                  result += tag['value'];
-                }
-              }
-
-              return result;
-              ",
-                type: :number,
-                order: :desc,
-                params: {
-                  terms: terms,
-                  tags: tags
-                }
-            }
-          },
-
-          from: 0,
-          size: 20
-        })
+        query.deep_merge!(strategy.call(question, terms))
+        query.merge!(from: 0, size: 10)
 
         retrieved_documents = results = Question.search(query)
         relevant_documents  = Question.where(stack_exchange_uuid: question.stack_exchange_questions_uuids)
@@ -96,91 +61,14 @@ module Yeast
         self.total_recall           += recall
         self.number_of_observations += 1
 
-        puts "Total Precision: #{total_precision / number_of_observations.to_f}"
-        puts "Total Recall: #{total_recall / number_of_observations.to_f}"
+
+        precision = total_precision / number_of_observations.to_f
+        recall = total_recall / number_of_observations.to_f
+
+        puts "Total Precision: #{precision}"
+        puts "Total Recall: #{recall}"
+        puts "F1 Measure: #{2 * (precision * recall)/(precision + recall)}"
       end
     end
   end
 end
-
-
-
-=begin
-    def publish(action, initiator, resource, options = {})
-      if resource.is_a? Question
-        return if resource.stack_exchange_duplicate
-        return unless Question.where(stack_exchange_duplicate: true).where("#{resource.stack_exchange_uuid} = ANY(stack_exchange_questions_uuids)").any?
-
-        question  = resource
-        text      = [question.title, question.text, question.tags.pluck(:name)].flatten.join(' ')
-        terms     = Question.probe.analyze(text: text, analyzer: :text)
-        tags      = question.author.profiles.where(property: :interest).order(:value).limit(10).map { |p| { name: p.targetable.name, value: p.value } }
-        results   = Question.search(
-          query: {
-            more_like_this: {
-              like_text: text,
-              fields: [:title, :text, :tags],
-              min_term_freq: 1
-            }
-          },
-
-          sort: {
-            _script: {
-              script: "
-              result = doc.score;
-              size   = _index.numDocs();
-
-              foreach(term : terms) {
-                frequency       = _index['title'][term].tf() + _index['text'][term].tf() + _index['tags'][term].tf();
-                total_frequency = _index['title'][term].ttf() + _index['text'][term].ttf() + _index['tags'][term].ttf();
-
-                if (frequency > 0 && total_frequency > 0) {
-                  result += frequency * log((size / total_frequency) + 1);
-                }
-              }
-
-              return result;
-
-              foreach(tag : tags) {
-                name = tag['name'];
-
-                if (_index['tags'][name].tf() > 0) {
-                  result += tag['value'];
-                }
-              }
-
-              return result;
-              ",
-                type: :number,
-                order: :desc,
-                params: {
-                  terms: terms,
-                  tags: tags
-                }
-            }
-          },
-
-          from: 0,
-          size: 10
-        )
-
-        retrieved_documents = results
-        relevant_documents  = Question.where(stack_exchange_duplicate: true).where("#{question.stack_exchange_uuid} = ANY(stack_exchange_questions_uuids)")
-
-        return unless retrieved_documents.size > 0
-
-        puts "Found #{results.size} questions"
-
-        precision = (retrieved_documents & relevant_documents).size / relevant_documents.size.to_f
-        recall    = (retrieved_documents & relevant_documents).size / retrieved_documents.size.to_f
-
-        self.total_precision        += precision
-        self.total_recall           += recall
-        self.number_of_observations += 1
-
-        puts "Total Precision: #{total_precision / number_of_observations.to_f}"
-        puts "Total Recall: #{total_recall / number_of_observations.to_f}"
-      end
-    end
-  end
-=end
