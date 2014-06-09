a390c2d1493b8cf8c8ac0c5d8cf82d5938eb46fa
MarekGrznar <marek.grznar@gmail.com>
2014-06-10 01:04:56 +0200

    Add tags searching feature test

---
+++ spec/features/search_tags_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'Search Tags' do
+  let!(:user) { create :user }
+
+  before :each do
+    Tag.probe.index.delete
+    Tag.probe.index.create
+
+    login_as user
+
+    create :tag, name: 'rails'
+    create :tag, name: 'ruby'
+    create :tag, name: 'linux'
+  end
+
+  it 'searches tags by name' do
+    visit root_path
+
+    click_link 'Tagy'
+
+    fill_in 'q', with: 'r'
+    click_button 'Hľadať'
+
+    expect(page).to     have_content('rails')
+    expect(page).to     have_content('ruby')
+    expect(page).not_to have_content('linux')
+
+
+    fill_in 'q', with: 'ux'
+    click_button 'Hľadať'
+
+    expect(page).to have_content('linux')
+  end
+end
