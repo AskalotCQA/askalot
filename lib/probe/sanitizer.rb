7f3835823a9adaeeb74b661435c34997881cf4c8
Samuel Molnar <molnar.samuel@gmail.com>
2014-06-09 22:58:42 +0200

    Add more complex sanitation for query

--- lib/probe/sanitizer.rb
+++ lib/probe/sanitizer.rb
@@ -2,9 +2,12 @@ module Probe
   module Sanitizer
     extend self
 
-    def sanitize_query(query)
-      # TODO (smolnar) implement
-      query.gsub(/[^a-z0-9?_]/i, "")
+    def sanitize_query(value)
+      value = value.gsub(/(\+|\-|&&|\||\||\(|\)|\{|\}|\[|\]|\^|~|\!|\\|\/|:)/) { |m| "\\#{m}" }
+
+      value[value.rindex('"')] = "\\\"" unless value.count('"') % 2 == 0
+
+      value
     end
   end
 end
