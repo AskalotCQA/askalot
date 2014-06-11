d86f8672ccc1d79b7f6c8ffe7a166112993a92e2
Samuel Molnar <molnar.samuel@gmail.com>
2014-06-11 12:21:24 +0200

    Fix migration

--- db/migrate/20140611004811_import_elasticsearch.rb
+++ db/migrate/20140611004811_import_elasticsearch.rb
@@ -1,7 +1,7 @@
 class ImportElasticsearch < ActiveRecord::Migration
   def up
     [Category, Question, Tag, User].each do |model|
-      Question.probe.index.import model.all, with: :bulk
+      model.probe.index.import model.all, with: :bulk
     end
   end
 
