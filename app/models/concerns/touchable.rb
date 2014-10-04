5f5cf05d28e17f23f0053f9e6d3c43405e82305c
Jozef Harinek <j.harinek@gmail.com>
2014-10-04 10:51:39 +0200

    Refactor attribute_changed? method

--- app/models/concerns/touchable.rb
+++ app/models/concerns/touchable.rb
@@ -17,9 +17,10 @@ module Touchable
   private
 
   def attribute_changed?(column)
-    attrs = ['favorites_count', 'votes_count', 'views_count', 'votes_difference', 'votes_lb_wsci_bp', ]
+    attributes = [:favorites_count, :votes_count, :views_count, :votes_difference, :votes_lb_wsci_bp ]
+
     if column == 'touched_at'
-      (self.changed & attrs).any? ? (return true) : (return super)
+      return (self.changed & attributes).any? ? true : super
     end
 
     super
