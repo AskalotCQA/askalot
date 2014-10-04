602a6eb2a5e51d51861cd43087f817e0853ea2e9
Jozef Harinek <j.harinek@gmail.com>
2014-10-04 11:08:06 +0200

    Add specs for voting, viewing, labeling Touchable

--- spec/models/concerns/touchable_spec.rb
+++ spec/models/concerns/touchable_spec.rb
@@ -14,5 +14,18 @@ shared_examples_for Touchable do
 
       expect(record.to_question.touched_at).not_to eql(timestamp)
     end
+
+    it 'does not update question touched_at attribute when voting, viewing, favoring' do
+      record        = create factory
+      old_timestamp = record.to_question.touched_at
+      user          = create :user
+
+      record.toggle_voteup_by! user   if record.respond_to? :toggle_voteup_by!
+      record.views_count += 1         if record.respond_to? :views_count
+      record.toggle_favoring_by! user if record.respond_to? :toggle_favoring_by!
+      record.toggle_labeling_by! user if record.respond_to? :toggle_labeling_by!
+
+      expect(record.to_question.touched_at).to eql(old_timestamp)
+    end
   end
 end
