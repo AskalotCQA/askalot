b40d22110ec0c8c4fc4437d0bfdd730368b5f31a
MarekGrznar <marek.grznar@gmail.com>
2014-10-05 19:30:35 +0200

    Add automatic notification sender to facebook

--- app/services/notifications/notifier.rb
+++ app/services/notifications/notifier.rb
@@ -4,11 +4,25 @@ module Notifications
 
     attr_accessor :factory
 
+
     def publish(action, initiator, resource, options = {})
       recipients = (Array.wrap(options[:for] || resource.watchers) - [initiator]).uniq
 
       recipients.each do |recipient|
-        factory.create!(action: action, initiator: initiator, recipient: recipient, resource: resource, anonymous: !!options[:anonymous])
+        message =  factory.create!(action: action, initiator: initiator, recipient: recipient, resource: resource, anonymous: !!options[:anonymous])
+
+        if message.resource.is_a?(Answer || Question || Comment)
+          if recipient.oauth_token
+            user = FbGraph::User.me(recipient.oauth_token).fetch
+            app = FbGraph::Application.new('308609915963601', :secret => '2c38cdee881763cedffe006d86a73c39')
+
+            user.notification!(
+              :access_token => app.get_access_token,
+              :href => 'http://askalot.fiit.stuba.sk',
+              :template => 'Askalot need your attention'
+            )
+          end
+        end
       end
     end
 
