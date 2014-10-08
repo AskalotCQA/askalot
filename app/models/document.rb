c2078979e178f4792609a93fa064123b8598dace
Jozef Harinek <j.harinek@gmail.com>
2014-10-08 16:12:34 +0200

    Add validations to Document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,13 +2,15 @@ class Document < ActiveRecord::Base
   include Deletable
   include Editable
 
-  TYPES=[:chunk, :question]
+  TYPES = [:text]
 
   belongs_to :group, counter_cache: true
 
   has_many :questions
 
-  validates :title,         presence: true, length: { maximum: 200 }
+  validates :title,   presence: true, length: { minimum: 2, maximum: 140 }
+  validates :content, presence: true, length: { minimum: 2 }
+
   validates :document_type, presence: true
 
   symbolize :document_type, in: TYPES
