a5b8ad4c27832d52aa46494be3e7eba29695c50e
Jozef Harinek <j.harinek@gmail.com>
2014-10-08 16:13:11 +0200

    Add validations to Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,13 +1,16 @@
 class Group < ActiveRecord::Base
   include Deletable
+  include Editable
 
-  VISIBILITIES=[:public, :student_only]
+  VISIBILITIES = [:public, :private]
 
   has_many :documents
 
   belongs_to :owner, class_name: :User
 
-  validates :title,      presence: true
+  validates :title,       presence: true, length: { minimum: 2, maximum: 140 }
+  validates :description, presence: true, length: { minimum: 2 }
+
   validates :visibility, presence: true
 
   symbolize :visibility, in: VISIBILITIES
