37739b5d3228351a187ec18bf5d30af42dc35dbc
Jozef Harinek <j.harinek@gmail.com>
2014-10-20 13:18:22 +0200

    Fix Touchable specs

--- spec/models/concerns/touchable_spec.rb
+++ spec/models/concerns/touchable_spec.rb
@@ -14,12 +14,12 @@ shared_examples_for Touchable do
       expect(record.to_question.touched_at).not_to eql(timestamp)
 
       record = create factory
-      timestamp = record.touched_at
+      timestamp = record.to_question.touched_at
 
       record.text += 'some additional text'
       record.save!
 
-      expect(record.touched_at).not_to eql(timestamp)
+      expect(record.to_question.touched_at).not_to eql(timestamp)
     end
 
     it 'does not update question touched_at attribute when voting, viewing, favoring' do
@@ -27,10 +27,15 @@ shared_examples_for Touchable do
       old_timestamp = record.to_question.touched_at
       user          = create :user
 
-      record.toggle_voteup_by! user   if record.respond_to? :toggle_voteup_by!
-      record.views_count += 1         if record.respond_to? :views_count
-      record.toggle_favoring_by! user if record.respond_to? :toggle_favoring_by!
-      record.toggle_labeling_by! user if record.respond_to? :toggle_labeling_by!
+      record.toggle_voteup_by! user          if record.respond_to? :toggle_voteup_by!
+      record.votes_count += 1                if record.respond_to? :votes_count
+      record.votes_difference += 1           if record.respond_to? :votes_difference
+      record.votes_lb_wsci_bp += 1           if record.respond_to? :votes_lb_wsci_bp
+      record.views_count += 1                if record.respond_to? :views_count
+      record.toggle_favoring_by! user        if record.respond_to? :toggle_favoring_by!
+      record.toggle_labeling_by! user, :best if record.respond_to? :toggle_labeling_by!
+
+      raise "Record does not respond to anything" unless record.changed?
 
       expect(record.to_question.touched_at).to eql(old_timestamp)
     end
