6f7151f244392c751dbcb6feeba0ed57a2efec2f
MarekGrznar <marek.grznar@gmail.com>
2014-10-21 10:22:12 +0200

    Add facebook application attributes to confing file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '308609915963601', '9d4459184d7a61afec6d216eda49f69d', scope: 'email,read_stream,user_likes,user_friends', display: 'page'
+  provider :facebook, ::Configuration.facebook.application.id , ::Configuration.facebook.application.secret, scope: 'email,read_stream,user_likes,user_friends', display: 'page'
 end
 
 OmniAuth.config.on_failure = Proc.new do |env|
