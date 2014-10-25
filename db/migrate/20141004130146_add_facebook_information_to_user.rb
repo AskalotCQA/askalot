c4d4ba27ce2d8793407018156087ec2a141b49d2
Pavol Zbell <pavol.zbell@gmail.com>
2014-10-26 01:05:26 +0200

    Reorder columns

--- db/migrate/20141004130146_add_facebook_information_to_user.rb
+++ db/migrate/20141004130146_add_facebook_information_to_user.rb
@@ -1,8 +1,9 @@
 class AddFacebookInformationToUser < ActiveRecord::Migration
   def change
-    add_column :users, :facebook_uid, :integer
     add_column :users, :omniauth_provider, :string
     add_column :users, :omniauth_token, :text
     add_column :users, :omniauth_token_expires_at, :datetime
+
+    add_column :users, :facebook_uid, :integer
   end
 end
