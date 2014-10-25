b54d8fc85db2afc02b86d6720cc1965edc29e525
MarekGrznar <marek.grznar@gmail.com>
2014-10-26 00:13:36 +0200

    Add migration to add facebook friend and likes to user

--- db/structure.sql
+++ db/structure.sql
@@ -880,7 +880,9 @@ CREATE TABLE users (
     facebook_uid integer,
     omniauth_provider character varying(255),
     omniauth_token text,
-    omniauth_token_expires_at timestamp without time zone
+    omniauth_token_expires_at timestamp without time zone,
+    facebook_friends text,
+    facebook_likes text
 );
 
 
@@ -2432,4 +2434,6 @@ INSERT INTO schema_migrations (version) VALUES ('20140513162801');
 INSERT INTO schema_migrations (version) VALUES ('20140611004811');
 
 INSERT INTO schema_migrations (version) VALUES ('20141004130146');
+
+INSERT INTO schema_migrations (version) VALUES ('20141023161800');
 
