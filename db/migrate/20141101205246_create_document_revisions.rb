a971fd16ac77beffe534b09be512b6f49826181e
Jozef Harinek <j.harinek@gmail.com>
2014-11-02 00:33:31 +0100

    Add migrations for group and document revisions

---
+++ db/migrate/20141101205246_create_document_revisions.rb
@@ -0,0 +1,16 @@
+class CreateDocumentRevisions < ActiveRecord::Migration
+  def change
+    create_table :document_revisions do |t|
+      t.references :document, null: false
+      t.references :editor,   null: false
+
+      t.string :title,   null: false
+      t.text   :content, null: false
+
+      t.timestamps
+    end
+
+    add_index :document_revisions, :document_id
+    add_index :document_revisions, :editor_id
+  end
+end
