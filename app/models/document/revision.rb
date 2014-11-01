1b894ffae55d89646f72df8f554b7030fe2b564c
Jozef Harinek <j.harinek@gmail.com>
2014-11-02 00:34:44 +0100

    Add document revision class

---
+++ app/models/document/revision.rb
@@ -0,0 +1,19 @@
+class Document
+  class Revision < ActiveRecord::Base
+    include Deletable
+
+    belongs_to :document
+    belongs_to :editor, class_name: :User
+
+    def self.create_revision!(editor, document)
+      revision          = Document::Revision.new
+      revision.editor   = editor
+      revision.document = document
+      revision.title    = document.title
+      revision.content  = document.content
+
+      revision.save!
+      revision
+    end
+  end
+end
