0f65c4774195fe1c85812abb621241b68d2afbf0
Jozef Harinek <j.harinek@gmail.com>
2014-11-03 20:32:55 +0100

    Add document reference to question revisions

--- app/models/question/revision.rb
+++ app/models/question/revision.rb
@@ -2,14 +2,17 @@ class Question
   class Revision < ActiveRecord::Base
     include Deletable
 
+    belongs_to :document
     belongs_to :question
     belongs_to :editor, class_name: :User
 
     def self.create_revision!(editor, question)
+      binding.pry
       revision          = Question::Revision.new
       revision.editor   = editor
       revision.question = question
-      revision.category = question.category.name
+      revision.category = question.category.name if question.category
+      revision.document = question.document      if question.document
       revision.tags     = question.labels.map(&:name)
       revision.title    = question.title
       revision.text     = question.text
