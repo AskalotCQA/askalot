c41f924108397f562ed453e26173cc1ce0e5e215
MarekGrznar <marek.grznar@gmail.com>
2014-11-11 00:00:15 +0100

    Bump version to 1.8.0.beta

--- config/version.rb
+++ config/version.rb
@@ -1,10 +1,10 @@
 module Askalot
   module VERSION
     MAJOR = 1
-    MINOR = 7
-    PATCH = 1
+    MINOR = 8
+    PATCH = 0
 
-    PRE = 'alpha'
+    PRE = 'beta'
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join '.'
   end
