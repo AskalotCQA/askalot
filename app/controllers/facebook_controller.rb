7cc38a02fad9f4146e6a9d91c7b2006acbe89d5d
Pavol Zbell <pavol.zbell@gmail.com>
2014-11-10 01:04:52 +0100

    Format FacebookController

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,18 +1,17 @@
 class FacebookController < ApplicationController
-  after_action :allow_facebook_iframe
   skip_before_action :verify_authenticity_token
 
+  after_action :allow_facebook_in_frame
+  
   layout 'layouts/facebook'
 
   def redirect
-    path = "/#{params[:path]}"
-
-    redirect_to path
+    redirect_to "/#{params[:path]}"
   end
 
   private
 
-  def allow_facebook_iframe
+  def allow_facebook_in_frame
     response.headers['X-Frame-Options'] = 'ALLOWALL'
   end
 end
