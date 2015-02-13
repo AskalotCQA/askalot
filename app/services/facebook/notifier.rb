e04edbe9f3a30fb83f35cf6ba0aac3c08bff22fb
MarekGrznar <marek.grznar@gmail.com>
2015-02-13 12:45:23 +0100

    Fix formating

--- app/services/facebook/notifier.rb
+++ app/services/facebook/notifier.rb
@@ -11,12 +11,13 @@ module Facebook
       application = FbGraph::Application.new(Configuration.facebook.application.id, secret: Configuration.facebook.application.secret)
 
       notifications.select { |notification| notification.recipient.omniauth_token }.each do |notification|
-        content = controller.render_to_string(partial: 'facebook/notification_content', locals: { notification: notification }).strip
-        link    = controller.render_to_string(partial: 'facebook/notification_link', locals: { notification: notification, content: content }).strip
-        token   = notification.recipient.omniauth_token
-
+        content          = controller.render_to_string(partial: 'facebook/notification_content', locals: { notification: notification }).strip
+        link             = controller.render_to_string(partial: 'facebook/notification_link', locals: { notification: notification, content: content }).strip
+        token            = notification.recipient.omniauth_token
         token_validation = application.debug_token token
+        
         next unless token_validation.is_valid
+
         FbGraph::User.me(token).fetch.notification! access_token: application.get_access_token, href: link, template: content
       end
     end
