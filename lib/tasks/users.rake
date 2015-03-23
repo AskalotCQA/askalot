016a8e1346c148dcc3866b512b9866099ffd2a05
Samuel Molnar <molnar.samuel@gmail.com>
2015-03-23 13:54:16 +0100

    Extract delivery logic to service rather than task

--- lib/tasks/users.rake
+++ lib/tasks/users.rake
@@ -1,8 +1,6 @@
 namespace :users do
   desc 'Send notifications to users via email'
   task notifications: :environment do
-    User.joins(:notifications).uniq.find_each do |user|
-      UserMailer.notifications(user, from: 1.day.ago).deliver!
-    end
+    UserMailerService.deliver_notifications!
   end
 end
---
+++ spec/services/mailers/user_mailer_service_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Mailers::UserMailerService do
+  describe '.deliver_notifications!' do
+    it 'delivers notifications' do
+      users = [
+        create(:user, send_email_notifications: false),
+        create(:user),
+        create(:user)
+      ]
+
+      Timecop.freeze do
+        Timecop.travel(2.days.ago) { create :notification, recipient: users[1], action: :create, resource: create(:question) }
+
+        create :notification, recipient: users[2], action: :create, resource: create(:question)
+
+        email = double(:email)
+
+        UserMailer.stub(:notifications).with(users[2], from: 1.days.ago) { email }
+
+        expect(email).to receive(:deliver!)
+
+        Mailers::UserMailerService.deliver_notifications!
+      end
+    end
+  end
+end
