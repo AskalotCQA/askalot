2ff9a9a317205062b099fb580653a2f8a5e861d0
Eva Hnilicová <eva.hni8@gmail.com>
2015-10-14 23:34:30 +0200

    Add creating new question with same category

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -43,6 +43,7 @@ class QuestionsController < ApplicationController
     @question = Question.new
 
     @question.document = Document.find(params[:document_id]) if params[:document_id]
+    @question.category_id = params[:category] if params[:category]
   end
 
   def create
