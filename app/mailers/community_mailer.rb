239d20ac418d14231c4f57e6d2479beadc6d81ab
Eva Hnilicová <eva.hni8@gmail.com>
2015-11-11 13:34:40 +0100

    Fix sending markdown in community emails

--- app/mailers/community_mailer.rb
+++ app/mailers/community_mailer.rb
@@ -21,16 +21,20 @@ class CommunityMailer < ActionMailer::Base
   helper VotesHelper
   helper WatchingsHelper
 
+  include MarkdownHelper
+
   layout 'mailer'
 
   def community_emails(email, user)
     @user = user
 
-    @content = email[:body].html_safe
+    @content = email[:body]
+    @content = render_markdown @content if email[:send_html_email]
+    @content = @content.html_safe unless email[:send_html_email]
 
     mail(to: @user.email, subject: email[:subject], content_type: email[:send_html_email] ? 'text/html' : 'text/plain') do |format|
       format.html if email[:send_html_email]
-      format.text
+      format.text unless email[:send_html_email]
     end
   end
 end
--- db/structure.sql
+++ db/structure.sql
@@ -128,8 +128,8 @@ CREATE TABLE answer_revisions (
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
     deleted boolean DEFAULT false NOT NULL,
-    deletor_id integer,
-    deleted_at timestamp without time zone
+    deleted_at timestamp without time zone,
+    deletor_id integer
 );
 
 
@@ -164,17 +164,17 @@ CREATE TABLE answers (
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
     votes_difference integer DEFAULT 0 NOT NULL,
-    stack_exchange_uuid integer,
     comments_count integer DEFAULT 0 NOT NULL,
     votes_count integer DEFAULT 0 NOT NULL,
     deleted boolean DEFAULT false NOT NULL,
     votes_lb_wsci_bp numeric(13,12) DEFAULT 0 NOT NULL,
     edited_at timestamp without time zone,
     editor_id integer,
-    deletor_id integer,
     deleted_at timestamp without time zone,
+    deletor_id integer,
     edited boolean DEFAULT false NOT NULL,
-    evaluations_count integer DEFAULT 0 NOT NULL
+    evaluations_count integer DEFAULT 0 NOT NULL,
+    stack_exchange_uuid integer
 );
 
 
@@ -310,8 +310,8 @@ CREATE TABLE comment_revisions (
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
     deleted boolean DEFAULT false NOT NULL,
-    deletor_id integer,
-    deleted_at timestamp without time zone
+    deleted_at timestamp without time zone,
+    deletor_id integer
 );
 
 
@@ -346,13 +346,13 @@ CREATE TABLE comments (
     text text NOT NULL,
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
-    stack_exchange_uuid integer,
     deleted boolean DEFAULT false NOT NULL,
     edited_at timestamp without time zone,
     editor_id integer,
-    deletor_id integer,
     deleted_at timestamp without time zone,
-    edited boolean DEFAULT false NOT NULL
+    deletor_id integer,
+    edited boolean DEFAULT false NOT NULL,
+    stack_exchange_uuid integer
 );
 
 
@@ -534,8 +534,8 @@ CREATE TABLE evaluations (
     created_at timestamp without time zone,
     updated_at timestamp without time zone,
     deleted boolean DEFAULT false NOT NULL,
-    deletor_id integer,
     deleted_at timestamp without time zone,
+    deletor_id integer,
     edited boolean DEFAULT false NOT NULL,
     edited_at timestamp without time zone,
     editor_id integer
@@ -602,8 +602,8 @@ CREATE TABLE favorites (
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
     deleted boolean DEFAULT false NOT NULL,
-    deletor_id integer,
     deleted_at timestamp without time zone,
+    deletor_id integer,
     stack_exchange_uuid integer
 );
 
@@ -750,8 +750,8 @@ CREATE TABLE labelings (
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
     deleted boolean DEFAULT false NOT NULL,
-    deletor_id integer,
     deleted_at timestamp without time zone,
+    deletor_id integer,
     stack_exchange_uuid integer
 );
 
@@ -894,8 +894,8 @@ CREATE TABLE question_revisions (
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
     deleted boolean DEFAULT false NOT NULL,
-    deletor_id integer,
     deleted_at timestamp without time zone,
+    deletor_id integer,
     document_id integer
 );
 
@@ -933,7 +933,6 @@ CREATE TABLE questions (
     updated_at timestamp without time zone NOT NULL,
     votes_difference integer DEFAULT 0 NOT NULL,
     anonymous boolean DEFAULT false NOT NULL,
-    stack_exchange_uuid integer,
     answers_count integer DEFAULT 0 NOT NULL,
     comments_count integer DEFAULT 0 NOT NULL,
     favorites_count integer DEFAULT 0 NOT NULL,
@@ -942,17 +941,18 @@ CREATE TABLE questions (
     slido_question_uuid integer,
     slido_event_uuid integer,
     deleted boolean DEFAULT false NOT NULL,
-    touched_at timestamp without time zone NOT NULL,
     votes_lb_wsci_bp numeric(13,12) DEFAULT 0 NOT NULL,
+    touched_at timestamp without time zone NOT NULL,
     edited_at timestamp without time zone,
     editor_id integer,
-    deletor_id integer,
     deleted_at timestamp without time zone,
+    deletor_id integer,
     edited boolean DEFAULT false NOT NULL,
-    stack_exchange_duplicate boolean,
-    stack_exchange_questions_uuids integer[],
     evaluations_count integer DEFAULT 0 NOT NULL,
     document_id integer,
+    stack_exchange_uuid integer,
+    stack_exchange_duplicate boolean,
+    stack_exchange_questions_uuids integer[],
     closed boolean DEFAULT false NOT NULL,
     closer_id integer,
     closed_at timestamp without time zone
@@ -1066,8 +1066,8 @@ CREATE TABLE taggings (
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
     deleted boolean DEFAULT false NOT NULL,
-    deletor_id integer,
     deleted_at timestamp without time zone,
+    deletor_id integer,
     author_id integer NOT NULL
 );
 
@@ -1214,7 +1214,6 @@ CREATE TABLE users (
     tumblr character varying(255),
     youtube character varying(255),
     role character varying(255) DEFAULT 'student'::character varying NOT NULL,
-    stack_exchange_uuid integer,
     answers_count integer DEFAULT 0 NOT NULL,
     comments_count integer DEFAULT 0 NOT NULL,
     favorites_count integer DEFAULT 0 NOT NULL,
@@ -1232,6 +1231,7 @@ CREATE TABLE users (
     facebook_friends text,
     facebook_likes text,
     documents_count integer DEFAULT 0 NOT NULL,
+    stack_exchange_uuid integer,
     send_email_notifications boolean DEFAULT true NOT NULL,
     read_notifications_thread boolean DEFAULT true NOT NULL,
     alumni boolean DEFAULT false NOT NULL
@@ -1267,8 +1267,8 @@ CREATE TABLE views (
     viewer_id integer NOT NULL,
     created_at timestamp without time zone NOT NULL,
     deleted boolean DEFAULT false NOT NULL,
-    deletor_id integer,
-    deleted_at timestamp without time zone
+    deleted_at timestamp without time zone,
+    deletor_id integer
 );
 
 
@@ -1304,8 +1304,8 @@ CREATE TABLE votes (
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
     deleted boolean DEFAULT false NOT NULL,
-    deletor_id integer,
     deleted_at timestamp without time zone,
+    deletor_id integer,
     stack_exchange_uuid integer
 );
 
