64100bbd827d9e0a96d09adf5678f3e15fa66e52
Ladislav Gallay <ladislav.gallay@lentil.sk>
2015-11-19 02:28:28 +0100

    Fix markdown for static pages

--- components/university/lib/university/engine.rb
+++ components/university/lib/university/engine.rb
@@ -1,5 +1,12 @@
 module University
   class Engine < ::Rails::Engine
     isolate_namespace University
+
+    config.to_prepare do
+      helpers = Shared.constants.select {|c| c.to_s.ends_with? 'Helper' }
+      helpers.each do |helper|
+        ApplicationController.helper ('Shared::' + helper.to_s).constantize
+      end
+    end
   end
 end
