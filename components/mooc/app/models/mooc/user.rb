dc37a900ae9241cef3b40fdbaf0ab2f5d5b3ca1e
Filip Jandura <filip.jandura@gmail.com>
2015-11-30 07:21:02 +0100

    Add lti user registration with email and session

--- components/mooc/app/models/mooc/user.rb
+++ components/mooc/app/models/mooc/user.rb
@@ -1,21 +1,21 @@
 module Mooc
-class User < Shared::User
-  ROLES = [:Student, :Administrator, :AskalotAdministrator]
+  class User < Shared::User
+    ROLES = [:Student, :Administrator, :AskalotAdministrator]
 
-    symbolize :role, in: ROLES
+      symbolize :role, in: ROLES
 
-    protected
+      protected
 
-    def self.create_without_confirmation!(attributes)
-      user = User.new(attributes)
+      def self.create_without_confirmation!(attributes)
+        user = User.new(attributes)
 
-      user.skip_confirmation!
-      user.save!
-      user
-    end
+        user.skip_confirmation!
+        user.save!
+        user
+      end
 
-    def password_required?
-      false
-    end
-end
+      def password_required?
+        false
+      end
+  end
 end
