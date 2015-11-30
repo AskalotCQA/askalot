69b5ecc9a35acaec26da9e352151b7b125b93c4a
Filip Jandura <filip.jandura@gmail.com>
2015-11-30 07:21:02 +0100

    Add lti user registration with email and session

--- components/mooc/config/routes.rb
+++ components/mooc/config/routes.rb
@@ -1,11 +1,3 @@
 Mooc::Engine.routes.draw do
-  post '/units', to: 'units#show'
-
-  devise_for :users, class_name: 'Mooc::User', controllers: { sessions: 'shared/sessions', registrations: 'shared/registrations' }, path: '', path_names: { sign_up: :join, sign_in: :login, sign_out: :logout }, module: :devise
-
-  resources :units, only: [:show]
-
-  resources :units, only: [] do
-    resources :questions, only: [:index, :create, :show, :update, :destroy]
-  end
+  post '/lti', to: 'lti#login'
 end
