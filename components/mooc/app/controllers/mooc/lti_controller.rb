a145700bbacadd4c6e25f2d20a2f244ec356c986
Filip Jandura <filip.jandura@gmail.com>
2015-12-03 23:03:22 +0100

    Update user registration with lti

--- components/mooc/app/controllers/mooc/lti_controller.rb
+++ components/mooc/app/controllers/mooc/lti_controller.rb
@@ -5,16 +5,14 @@ class LtiController < ApplicationController
 
   skip_before_filter  :verify_authenticity_token
 
-  $oauth_creds = {"test" => "secret", "testing" => "supersecret"}
+  $oauth_creds = { Shared::Configuration.oauth.consumer_key => Shared::Configuration.oauth.consumer_secret }
 
   def login
     redirect_to 'errors#show' unless authorize!
 
-    u = User.find_by(login: "user#{params['user_id']}")
-
-    #TODO (janduraf) add specifis roles to DB according edX system role: params['roles']
-    u = User.create_without_confirmation! login: "user#{params['user_id']}", nick: params['lis_person_sourcedid'],
-                                          email: params['lis_person_contact_email_primary'] if u.nil?
+    u = User.find_by(login: params['user_id'])
+    u = User.create_without_confirmation! login: params['user_id'], nick: params['lis_person_sourcedid'],
+                                          email: params['lis_person_contact_email_primary'], role: params['roles'] if u.nil?
 
     sign_in(:user, u) unless signed_in?
     redirect_to '/questions'
@@ -23,44 +21,19 @@ class LtiController < ApplicationController
   protected
 
   def authorize!
-    if key = params['oauth_consumer_key']
-      if secret = $oauth_creds[key]
-        @tp = IMS::LTI::ToolProvider.new(key, secret, params)
-      else
-        @tp = IMS::LTI::ToolProvider.new(nil, nil, params)
-        @tp.lti_msg = "Your consumer didn't use a recognized key."
-        @tp.lti_errorlog = "You did it wrong!"
-        show_error "Consumer key wasn't recognized"
-        return false
-      end
+    return false unless key = params['oauth_consumer_key']
+
+    if secret = $oauth_creds[key]
+      @tp = IMS::LTI::ToolProvider.new(key, secret, params)
     else
-      show_error "No consumer key"
+      @tp = IMS::LTI::ToolProvider.new(nil, nil, params)
+
       return false
     end
 
-    if !@tp.valid_request?(request)
-      show_error "The OAuth signature was invalid"
-      return false
-    end
-
-    if Time.now.utc.to_i - @tp.request_oauth_timestamp.to_i > 60*60
-      show_error "Your request is too old."
-      return false
-    end
-
-    # this isn't actually checking anything like it should, just want people
-    # implementing real tools to be aware they need to check the nonce
-    if was_nonce_used_in_last_x_minutes?(@tp.request_oauth_nonce, 60)
-      show_error "Why are you reusing the nonce?"
-      return false
-    end
-
-    return true
-  end
-
-  def was_nonce_used_in_last_x_minutes?(nonce, minutes=60)
-    # some kind of caching solution or something to keep a short-term memory of used nonces
-    false
+    return false unless @tp.valid_request?(request)
+    return false if Time.now.utc.to_i - @tp.request_oauth_timestamp.to_i > 60*60
+    true
   end
 end
 end
