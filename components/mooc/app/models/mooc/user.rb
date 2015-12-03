9ff316b0939441af387a044875a3c19dc488fe48
Filip Jandura <filip.jandura@gmail.com>
2015-12-03 23:03:22 +0100

    Update user registration with lti

--- components/mooc/app/models/mooc/user.rb
+++ components/mooc/app/models/mooc/user.rb
@@ -1,5 +1,18 @@
 module Mooc
 class User < Shared::User
+  ROLES = [:Student, :Administrator, :AskalotAdministrator]
+
+  symbolize :role, in: ROLES
+
+  protected
+
+  def self.create_without_confirmation!(attributes)
+    user = User.new(attributes)
+
+    user.skip_confirmation!
+    user.save!
+    user
+  end
 
   def password_required?
     false
--- components/mooc/config/routes.rb
+++ components/mooc/config/routes.rb
@@ -1,3 +1,5 @@
 Mooc::Engine.routes.draw do
   post '/lti', to: 'lti#login'
+
+  devise_for :users, class_name: 'Mooc::User', controllers: { sessions: 'shared/sessions', registrations: 'shared/registrations' }, path: '', path_names: { sign_up: :join, sign_in: :login, sign_out: :logout }, module: :devise
 end
