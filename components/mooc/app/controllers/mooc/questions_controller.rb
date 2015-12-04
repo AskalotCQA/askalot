8205d6ea74dbd84a95a65512cf3e32a179f6f022
Filip Jandura <filip.jandura@gmail.com>
2015-12-04 12:55:52 +0100

    Create unit view

--- components/mooc/app/controllers/mooc/questions_controller.rb
+++ components/mooc/app/controllers/mooc/questions_controller.rb
@@ -1,18 +1,9 @@
 module Mooc
   class QuestionsController < Shared::QuestionsController
-    load_and_authorize_resource
-
     layout 'mooc/unit'
 
     def show
-      @labels  = @question.labels
-      @answers = @question.ordered_answers
-      @answer  = Shared::Answer.new(question: @question)
-      @view    = @question.views.create! viewer: current_user
-
-      @question.increment :views_count
-
-      dispatch_event :create, @view, for: @question.watchers
+      super
       render 'mooc/units/questions/show'
     end
   end
