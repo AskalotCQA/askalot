e3bb7147a49c8371a5c73719e381c77e389a253e
Filip Jandura <filip.jandura@gmail.com>
2015-12-04 09:38:43 +0100

    Edit config questions resource

--- components/university/config/routes.rb
+++ components/university/config/routes.rb
@@ -2,4 +2,10 @@ University::Engine.routes.draw do
   resources :groups, only: [:index, :new, :create, :show, :update, :destroy] do
     resources :documents, only: [:create]
   end
+
+  resources :documents, only: [:update, :destroy] do
+    resources :questions, only: [] do
+      get :document_index, on: :collection, as: :index
+    end
+  end
 end
