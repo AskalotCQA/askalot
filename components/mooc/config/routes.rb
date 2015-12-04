3ea5f5dd51e216f889a2dcf659dc448ca37a8c31
Filip Jandura <filip.jandura@gmail.com>
2015-12-04 09:35:26 +0100

    Update mooc config with questions

--- components/mooc/config/routes.rb
+++ components/mooc/config/routes.rb
@@ -2,4 +2,12 @@ Mooc::Engine.routes.draw do
   post '/lti', to: 'lti#login'
 
   devise_for :users, class_name: 'Mooc::User', controllers: { sessions: 'shared/sessions', registrations: 'shared/registrations' }, path: '', path_names: { sign_up: :join, sign_in: :login, sign_out: :logout }, module: :devise
+
+  resources :units, only: [:index, :new, :create, :show, :update, :destroy]
+
+  resources :units, only: [:update, :destroy] do
+    resources :questions, only: [] do
+      get :document_index, on: :collection, as: :index
+    end
+  end
 end
