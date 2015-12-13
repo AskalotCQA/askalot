7ae7209b392bd987e133d006c5dbef6f7355636d
Filip Jandura <filip.jandura@gmail.com>
2015-12-14 00:38:02 +0100

    Add redirect to global askalot on tags

--- components/mooc/app/controllers/mooc/questions_controller.rb
+++ components/mooc/app/controllers/mooc/questions_controller.rb
@@ -1,16 +1,19 @@
 module Mooc
   class QuestionsController < Shared::QuestionsController
-    load_and_authorize_resource
-
     layout 'mooc/unit'
 
     def show
+      @question = Mooc::Question.find(params[:id])
+
+      authorize! :view, @question
+
       @labels  = @question.labels
       @answers = @question.ordered_answers
       @answer  = Shared::Answer.new(question: @question)
       @view    = @question.views.create! viewer: current_user
 
       @question.increment :views_count
+      @page_url = params[:page_url]
 
       dispatch_event :create, @view, for: @question.watchers
       render 'mooc/units/questions/show'
