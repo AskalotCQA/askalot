e07ab9a37e54b1ab644e7d9a8a38d16f41f15c88
Adrian Huna <huna.adrian@gmail.com>
2016-01-02 14:27:09 +0100

    Fix engines mounting order

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Askalot::Application.routes.draw do
+  mount Shared::Engine => Rails.application.config.relative_url_root || '/', as: 'shared'
   mount University::Engine => Rails.application.config.relative_url_root || '/', as: 'university' if Rails.module == 'university'
   mount Mooc::Engine => Rails.application.config.relative_url_root || '/', as: 'mooc' if Rails.module == 'mooc'
-  mount Shared::Engine => Rails.application.config.relative_url_root || '/', as: 'shared'
 end
