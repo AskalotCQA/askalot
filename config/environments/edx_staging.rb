632873ec30181b8aba1da90feda19ee473f4bf8a
Ivan Srba <ivan.srba@stuba.sk>
2016-01-02 22:23:55 +0100

    Add mooc.js/css and public.js to precompile list

--- config/environments/edx_staging.rb
+++ config/environments/edx_staging.rb
@@ -59,7 +59,7 @@ Askalot::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += ["*/mooc.css", "*/mooc.js", "*/public.js"]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
