79e63644e28583bcc61628585f90c883aea0553b
Martin Černák <cernakmartin3@gmail.com>
2016-03-13 16:37:28 +0100

    Add in_questions flag to question_prefix_label

--- components/mooc/app/helpers/mooc/questions_helper.rb
+++ components/mooc/app/helpers/mooc/questions_helper.rb
@@ -1,7 +1,7 @@
 module Mooc::QuestionsHelper
   extend Shared::QuestionsHelper
 
-  def question_prefix_label(label, page_url, options = {})
+  def question_prefix_label(label, page_url, options = {}, in_questions = false)
     model, tags, classes = question_label_attributes label
 
     filter = ((params[:tags] || '').split(',') + tags).uniq.join(',')
@@ -9,7 +9,13 @@ module Mooc::QuestionsHelper
     options.merge!(class: classes, target: '_parent')
     options.deep_merge! class: classes, data: { id: filter } unless filter.blank?
 
-    href = (page_url ? page_url + '?redirect=' : '') + shared.questions_path(tags: filter)
+    if label.class.to_s == 'Shared::Category'
+      href = '#'
+      href = mooc.unit_path(id: label.id) unless in_questions
+    else
+      href = (page_url ? page_url + '?redirect=' : '') + shared.questions_path(tags: filter)
+    end
+
     link_to question_label_name(label), href, analytics_attributes(model, :click, label.name).deep_merge(options)
   end
 end
