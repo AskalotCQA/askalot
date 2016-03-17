bd5126e96a556750bf1d47ec2a163a6b6db5f1f2
Ivan Srba <ivan.srba@stuba.sk>
2016-03-17 21:13:35 +0100

    Update structure.sql and timestamps deactivation in migrations
    [skip ci]

--- components/shared/db/migrate/20151108201548_migrate_question_to_new_categories.rb
+++ components/shared/db/migrate/20151108201548_migrate_question_to_new_categories.rb
@@ -1,6 +1,6 @@
 class MigrateQuestionToNewCategories < ActiveRecord::Migration
   def up
-    ActiveRecord::Base.disable_timestamps
+    ActiveRecord::Base.record_timestamps = false
 
     root = Shared::Category.roots.find_by name: 'root'
     last_year = root.children.empty? ? '' : root.children.sort_by(&:name).last.name
