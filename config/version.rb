19406eb58e294fb893bdb4bfb21ecd3c2c5fe4f1
Ladislav Gallay <ladislav.gallay@lentil.sk>
2016-03-18 17:22:16 +0100

    Change version to 3.0.0.gama

--- config/version.rb
+++ config/version.rb
@@ -1,10 +1,10 @@
 module Askalot
   module VERSION
-    MAJOR = 2
-    MINOR = 2
-    PATCH = 2
+    MAJOR = 3
+    MINOR = 0
+    PATCH = 0
 
-    PRE = 'alpha'
+    PRE = 'gama'
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact * '.'
   end
