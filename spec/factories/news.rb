96e7e6e379448ae12c5ba8e903700dd042ba2674
Eva Hnilicová <eva.hni8@gmail.com>
2016-03-18 15:49:17 +0100

    Add test for add news [skip ci]

--- spec/factories/news.rb
+++ spec/factories/news.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
-  factory :news do
-    title "MyString"
-    description "MyText"
-    show false
+  factory :news, class: Shared::New do
+    title 'Test News'
+    description 'News'
+    show true
   end
 end
