110aa632694115558b50e0bf400c80491ff23baa
Martin Černák <cernakmartin3@gmail.com>
2016-04-11 01:36:31 +0200

    Structure from gama

--- components/university/db/structure.sql
+++ components/university/db/structure.sql
@@ -255,7 +255,7 @@ CREATE TABLE categories (
     full_public_name character varying(255),
     public_tags character varying(255)[] DEFAULT '{}'::character varying[],
     shared boolean DEFAULT true,
-    askable boolean DEFAULT true,
+    askable boolean DEFAULT false,
     description text
 );
 
@@ -3471,6 +3471,4 @@ INSERT INTO schema_migrations (version) VALUES ('20160306211255');
 INSERT INTO schema_migrations (version) VALUES ('20160307103948');
 
 INSERT INTO schema_migrations (version) VALUES ('20160313091803');
-
-INSERT INTO schema_migrations (version) VALUES ('20160417130646');
 
