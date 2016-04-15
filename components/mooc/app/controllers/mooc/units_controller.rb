b13d83caf754ade88dd8c7b39967077d541acb93
Ivan Srba <ivan.srba@stuba.sk>
2016-04-15 14:16:42 +0200

    Move 'account not created yet' error to locales
    [skip ci]

--- components/mooc/app/controllers/mooc/units_controller.rb
+++ components/mooc/app/controllers/mooc/units_controller.rb
@@ -70,7 +70,7 @@ module Mooc
       begin
         user = User.find_by(login: params['user_id'])
 
-        raise 'Your user account is not registered yet and user information are not provided in this request (probably you are accessing Askalot unit view for the first time from edX Studio). Please, visit this unit directly from edX course to automatically register your account.' if user.nil? && (!params['lis_person_sourcedid'] || !params['lis_person_contact_email_primary'])
+        raise t('unit.error.account_not_created_yet') if user.nil? && (!params['lis_person_sourcedid'] || !params['lis_person_contact_email_primary'])
 
         user_attributes = { login: params['user_id'], nick: params['lis_person_sourcedid'],
             email: params['lis_person_contact_email_primary'], role: params['roles'].downcase }
