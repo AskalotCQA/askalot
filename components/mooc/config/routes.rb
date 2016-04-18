515043e7196239d369073ee9feef8a15af23f702
Filip Jandura <filip.jandura@gmail.com>
2016-04-18 14:38:12 +0200

    Fix configuration.yml

--- components/mooc/config/routes.rb
+++ components/mooc/config/routes.rb
@@ -1,23 +1,23 @@
 Mooc::Engine.routes.draw do
   scope '/(:context)/' do
-  post '/units', to: 'units#show'
-  get '/units/error', to: 'units#error'
+    post '/units', to: 'units#show'
+    get '/units/error', to: 'units#error'
 
-  devise_for :users, class_name: 'Mooc::User', controllers: { sessions: 'shared/sessions', registrations: 'shared/registrations' }, path: '', path_names: { sign_up: :join, sign_in: :login, sign_out: :logout }, module: :devise
+    devise_for :users, class_name: 'Mooc::User', controllers: { sessions: 'shared/sessions', registrations: 'shared/registrations' }, path: '', path_names: { sign_up: :join, sign_in: :login, sign_out: :logout }, module: :devise
 
-  resources :units, only: [:show] do
-    resources :questions, only: [:index, :create, :show, :update, :destroy]
-  end
-
-  match '/parser' => "parser#parser", via: [:post]
-  match '/parser' => "parser#options", via: [:options]
-
-  namespace :teacher_administration do
-    root 'categories#index'
-
-    resources :categories, except: [:show, :destroy] do
-      post 'settings' => 'categories#update_settings', on: :collection
+    resources :units, only: [:show] do
+      resources :questions, only: [:index, :create, :show, :update, :destroy]
     end
-  end
+
+    match '/parser' => "parser#parser", via: [:post]
+    match '/parser' => "parser#options", via: [:options]
+
+    namespace :teacher_administration do
+      root 'categories#index'
+
+      resources :categories, except: [:show, :destroy] do
+        post 'settings' => 'categories#update_settings', on: :collection
+      end
+    end
   end
 end
--- spec/support/shared/context.rb
+++ spec/support/shared/context.rb
@@ -1,6 +1,5 @@
 class ActionDispatch::Routing::RouteSet
   def default_url_options(options={})
     Rails.module.mooc? ? { context: Shared::Context::Manager.current_context } : {}
-    {}
   end
 end
