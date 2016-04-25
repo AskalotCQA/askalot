5f2df40ab555a0dd8ea066e8c1450a777b250981
Adrián Huňa <huna.adrian@gmail.com>
2016-04-25 12:26:36 +0200

    Stylize dashboard icons
    [skip ci]

--- components/shared/app/helpers/shared/dashboard_helper.rb
+++ components/shared/app/helpers/shared/dashboard_helper.rb
@@ -1,9 +1,9 @@
 module Shared::DashboardHelper
-  def dashboard_link(icon, number, description, path)
+  def dashboard_link(icon, number, description, path, type)
     html = <<-HTML
       <div class="col-lg-6 col-md-12 col-sm-6 col-xs-12">
         <a href="#{path}">
-          <div class="link-icon">#{fa_icon icon}</div>
+          <div class="link-icon #{type}">#{fa_icon icon}</div>
           <div class="link-text">
             <div class="number">#{number}</div>
             <div class="description">#{description}</div>
