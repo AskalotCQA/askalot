89677fd97dffc9ef7023fa3e15193c44cd340d89
Tibor Žuffa <tiborzuffa@gmail.com>
2016-04-25 10:53:18 +0200

    Show only new questions from dashboard [skip ci]

--- components/shared/app/controllers/concerns/shared/dashboard/questions.rb
+++ components/shared/app/controllers/concerns/shared/dashboard/questions.rb
@@ -1,60 +1,69 @@
 module Shared::Dashboard::Questions
   extend ActiveSupport::Concern
 
-  def new_questions(context)
+  def dashboard_questions(context)
     Shared::Question.in_context(context)
   end
 
-  def new_answers(context)
+  def dashboard_answers(context)
     Shared::Answer.in_context(context)
   end
 
-  def new_question_comments(questions)
+  def dashboard_question_comments(questions)
     Shared::Comment.for(:question).where("commentable_id IN (?)", questions.pluck(:id))
   end
 
-  def new_answer_comments(answers)
+  def dashboard_answer_comments(answers)
     Shared::Comment.for(:answer).where("commentable_id IN (?)", answers.pluck(:id))
   end
 
-  def new_questions_watched(questions, user)
+  def dashboard_questions_watched(questions, user)
     questions.joins(:watchings).where("watcher_id = ?", user.id)
   end
 
-  def new_answers_watched(answers, questions)
+  def dashboard_answers_watched(answers, questions)
     answers.where("answers.question_id IN (?)", questions.pluck(:id))
   end
 
-  def new_question_comments_watched(questions)
+  def dashboard_question_comments_watched(questions)
     Shared::Comment.for(:question).where("commentable_id IN (?)", questions.pluck(:id))
   end
 
-  def new_answer_comments_watched(answers)
+  def dashboard_answer_comments_watched(answers)
     Shared::Comment.for(:answer).where("commentable_id IN (?)", answers.pluck(:id))
   end
 
   def questions_by_dashboard_param(from_dashboard, context, user)
     case from_dashboard
       when :new_questions
-        new_questions(context)
+        questions = dashboard_questions(context)
+        only_new(questions, user)
       when :new_answers
-        context_answers = new_answers(context)
+        context_answers = dashboard_answers(context)
+        context_answers = only_new(context_answers, user)
         questions_by_answers(context_answers)
       when :new_questions_in_watched_categories
-        context_questions = new_questions(context)
-        new_questions_watched(context_questions, user)
+        context_questions = dashboard_questions(context)
+        context_questions = dashboard_questions_watched(context_questions, user)
+        only_new(context_questions, user)
       when :new_answers_in_watched_categories
-        context_questions = new_questions(context)
-        context_answers = new_answers(context)
-        context_questions = new_questions_watched(context_questions, user)
-        context_answers = new_answers_watched(context_answers, context_questions)
+        context_questions = dashboard_questions(context)
+        context_answers = dashboard_answers(context)
+        context_questions = dashboard_questions_watched(context_questions, user)
+        context_answers = dashboard_answers_watched(context_answers, context_questions)
+        context_answers = only_new(context_answers, user)
         questions_by_answers(context_answers)
       else
-        new_questions(context)
+        questions = dashboard_questions(context)
+        only_new(questions, user)
     end
   end
 
   def questions_by_answers(answers)
     Shared::Question.joins(:answers).where(answers: {id: answers.pluck(:id) })
+  end
+
+  def only_new(query, user)
+    query.where("#{query.table_name}.created_at >= ?", user.dashboard_last_sign_in_at)
   end
 end
--- components/shared/app/controllers/shared/static_pages_controller.rb
+++ components/shared/app/controllers/shared/static_pages_controller.rb
@@ -20,19 +20,19 @@ class StaticPagesController < ApplicationController
     @question = Shared::Questions::ToAnswerRecommender.next
     @context = Shared::Context::Manager.current_context
 
-    context_questions = new_questions(@context)
-    context_answers = new_answers(@context)
-    context_question_comments = new_question_comments(context_questions)
-    context_question_answers = new_answer_comments(context_answers)
+    context_questions = dashboard_questions(@context)
+    context_answers = dashboard_answers(@context)
+    context_question_comments = dashboard_question_comments(context_questions)
+    context_question_answers = dashboard_answer_comments(context_answers)
 
     @new_questions = dashboard_count context_questions
     @new_answers = dashboard_count context_answers
     @new_comments = dashboard_count(context_question_comments) + dashboard_count(context_question_answers)
 
-    context_questions = new_questions_watched(context_questions, current_user)
-    context_answers = new_answers_watched(context_answers, context_questions)
-    context_question_comments = new_question_comments_watched(context_questions)
-    context_question_answers = new_answer_comments_watched(context_answers)
+    context_questions = dashboard_questions_watched(context_questions, current_user)
+    context_answers = dashboard_answers_watched(context_answers, context_questions)
+    context_question_comments = dashboard_question_comments_watched(context_questions)
+    context_question_answers = dashboard_answer_comments_watched(context_answers)
 
     @new_questions_watched = dashboard_count context_questions
     @new_answers_watched = dashboard_count context_answers
