b09a63d88cafffc35a29da2257f1f0d2c194eae0
Adrián Huňa <huna.adrian@gmail.com>
2016-08-16 13:40:33 +0200

    Make category labels in unit view consistent

--- components/mooc/app/helpers/mooc/questions_helper.rb
+++ components/mooc/app/helpers/mooc/questions_helper.rb
@@ -10,7 +10,7 @@ module Mooc::QuestionsHelper
     options.deep_merge! class: classes, data: { id: filter } unless filter.blank?
 
     if label.class.to_s == 'Shared::Category'
-      return content_tag(:div, label.name, :class => 'label label-primary') if in_questions_index
+      return content_tag(:div, label.full_public_name, :class => 'label label-primary') if in_questions_index
 
       href = mooc.unit_path(id: label.id)
     else
