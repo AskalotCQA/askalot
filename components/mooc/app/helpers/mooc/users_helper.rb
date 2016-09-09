5ba6263d1d7a4f5baaf93f939e5f738f4211c7cc
Adrián Huňa <huna.adrian@gmail.com>
2016-09-09 08:47:30 +0200

    Fix anonymous question in unit view and shared sessions between environments

--- components/mooc/app/helpers/mooc/users_helper.rb
+++ components/mooc/app/helpers/mooc/users_helper.rb
@@ -4,7 +4,8 @@ module Mooc::UsersHelper
   def link_to_user_unit(user, page_url = nil, options = {})
     authorable = options.delete(:authorable)
     user = (user == authorable.author ? authorable.author_or_anonymous : user) if authorable
-    href = (page_url ? page_url + '#' : '') + shared.user_path(user.nick)
+    href = (page_url ? page_url + '#' : '')
+    href = href + shared.user_path(user.nick) unless user == :anonymous
 
     options.merge!(target: '_parent')
 
--- components/shared/config/initializers/session_store.rb
+++ components/shared/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Askalot::Application.config.session_store :cookie_store, key: '_Askalot_session'
+Askalot::Application.config.session_store :cookie_store, key: "#{Rails.env}_Askalot_session"
