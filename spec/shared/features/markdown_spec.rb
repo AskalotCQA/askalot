f4e8373370228b0defe5e1f59ff3beb37125f4bd
Adrian Huna <huna.adrian@gmail.com>
2016-10-19 12:36:42 +0200

    Escape characters in math environment

--- spec/shared/features/markdown_spec.rb
+++ spec/shared/features/markdown_spec.rb
@@ -5,7 +5,6 @@ describe 'Markdown', type: :feature do
   let!(:question) { create :question, :with_tags }
   let!(:category) { create :category }
   let(:text) { '\\\\(20 + 1_x/x_2^{2} \\\\), \\\\[ \frac{1}{n^{2}} \\\\],'\
-               '[mathjaxinline]1/x^{2}@20[/mathjaxinline], [mathjax]\\frac{1@10}{n^{2}}[/mathjax],'\
                '$\\frac{1@10}{n^{2}}$ , $$\\frac{1@10}{n^{2}}$$' }
 
   before :each do
@@ -40,7 +39,7 @@ describe 'Markdown', type: :feature do
       wait_for_remote
 
       within '.markdown-preview' do
-        expect(page).to have_selector('span.MathJax_SVG', count: 6)
+        expect(page).to have_selector('span.MathJax_SVG', count: 4)
       end
     end
 
@@ -117,7 +116,7 @@ describe 'Markdown', type: :feature do
       click_button 'Opýtať'
 
       within '.question-content' do
-        expect(page).to have_selector('span.MathJax_SVG', count: 6)
+        expect(page).to have_selector('span.MathJax_SVG', count: 4)
       end
     end
 
@@ -261,7 +260,7 @@ describe 'Markdown', type: :feature do
       expect(page).to have_content('Odpoveď bola úspešne pridaná.')
 
       within '#question-answers' do
-        expect(page).to have_selector('span.MathJax_SVG', count: 6)
+        expect(page).to have_selector('span.MathJax_SVG', count: 4)
       end
     end
   end
@@ -316,7 +315,7 @@ describe 'Markdown', type: :feature do
       end
 
       within '#question-comments' do
-        expect(page).to have_selector('span.MathJax_SVG', count: 6)
+        expect(page).to have_selector('span.MathJax_SVG', count: 4)
       end
     end
   end
