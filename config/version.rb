a493f47240899a160de42f44cb262f92ac877ccd
Ivan Srba <ivan.srba@stuba.sk>
2017-01-07 22:04:15 +0100

    "Bump version to 3.1.0.alpha [skip ci]"

--- config/version.rb
+++ config/version.rb
@@ -1,10 +1,10 @@
 module Askalot
   module VERSION
     MAJOR = 3
-    MINOR = 0
-    PATCH = 6
+    MINOR = 1
+    PATCH = 0
 
-    PRE = 'gama'
+    PRE = 'alpha'
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact * '.'
   end
