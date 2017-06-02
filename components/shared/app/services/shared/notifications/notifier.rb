6ee97ee336086923d631f35dd727e50b1fddadfd
Adrian Huna <huna.adrian@gmail.com>
2017-06-02 18:48:59 +0200

    Do not create notification for update action if it is too early after post creation

--- components/shared/app/services/shared/notifications/notifier.rb
+++ components/shared/app/services/shared/notifications/notifier.rb
@@ -5,6 +5,8 @@ module Shared::Notifications
     attr_accessor :factory
 
     def publish(action, initiator, resource, options = {})
+      return if action == :update && too_new_since_resource_creation?(resource) || too_new_since_last_notification?(resource)
+
       recipients = (Array.wrap(options[:for] || resource.watchers) - [initiator]).uniq
       context = Shared::Context::Manager.current_context_id
       notifications = []
@@ -21,6 +23,20 @@ module Shared::Notifications
 
     def factory
       @factory ||= Shared::Notification
+    end
+
+    private
+
+    def too_new_since_resource_creation?(resource)
+      (Time.current - resource.created_at) < 300 # 300 seconds
+    end
+
+    def too_new_since_last_notification?(resource)
+      last_notification = resource.notifications.where(action: :update).order(created_at: :desc).first
+
+      return false if last_notification.nil?
+
+      (Time.current - last_notification.created_at) < 60
     end
   end
 end
