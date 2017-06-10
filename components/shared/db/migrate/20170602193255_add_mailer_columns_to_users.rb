3382d9d771ad37199f51fef83f68d764d61af5b0
Ivan Srba <ivan.srba@stuba.sk>
2017-06-10 17:57:56 +0200

    Make email notifications instant by default + text fix

--- components/shared/db/migrate/20170602193255_add_mailer_columns_to_users.rb
+++ components/shared/db/migrate/20170602193255_add_mailer_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddMailerColumnsToUsers < ActiveRecord::Migration
   def up
-    add_column :users, :send_mail_notifications_frequency, :string, default: 'daily'
+    add_column :users, :send_mail_notifications_frequency, :string, default: 'instantly'
     add_column :users, :last_mail_notification_sent_at, :datetime
     add_column :users, :mail_notification_delay, :integer, default: 0
 
