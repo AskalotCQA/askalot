956bc24faf0b417f7c7e465f85c86e7f22456b57
Ivan Srba <ivan.srba@stuba.sk>
2017-06-11 09:50:06 +0200

    Fix incorrect delay info in instant emails

--- components/shared/app/services/shared/notifications/utility.rb
+++ components/shared/app/services/shared/notifications/utility.rb
@@ -12,6 +12,10 @@ module Shared::Notifications
       Shared::Notification.for(user).unread.where('created_at >= ?', notifications_since(user))
     end
 
+    def unread_notifications_in_interval(user)
+      Shared::Notification.for(user).unread.where('created_at >= ?', Shared::Configuration.mailer.check_unread_notifications_hours.hours.ago)
+    end
+
     def notifications_since(user)
       user.send_mail_notifications_frequency == 'daily' ? 1.day.ago : user.last_mail_notification_sent_at
     end
@@ -24,11 +28,9 @@ module Shared::Notifications
     def update_delay(user)
       return unless user.send_mail_notifications_frequency == 'instantly'
 
-      unread_notifications_in_interval = Shared::Notification.for(user).unread.where('created_at >= ?', Shared::Configuration.mailer.check_unread_notifications_hours.hours.ago)
-
       delay_from_config = Shared::Configuration.mailer.notification_delay
 
-      if unread_notifications_in_interval.any?
+      if unread_notifications_in_interval(user).any?
         user.update(mail_notification_delay: delay_from_config) if user.mail_notification_delay != delay_from_config
       else
         user.update(mail_notification_delay: 0) if user.mail_notification_delay != 0
