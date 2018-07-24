aa2d5d9c3eac76ed9da2daa015b4dbe023beb48f
Ivan Srba <ivan.srba@stuba.sk>
2018-07-24 14:35:52 +0200

    Fix rack timeout setting

--- components/shared/config/initializers/timeout.rb
+++ components/shared/config/initializers/timeout.rb
@@ -1,2 +1,2 @@
 # TODO (smolnar) consider decreasing the value
-Rack::Timeout.timeout = 60 if Rails.env_type.production?
+Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 60 if Rails.env_type.production?
